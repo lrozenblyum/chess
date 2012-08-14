e48ad60b31b3fb51f2a73acaadfbdadce452eace
Leonid Rozenblyum <lrozenblyum@gmail.com>
2012-08-14 22:45:38 +0300

    Edition via Idea

--- src/com/leokom/chess/MainRunner.java
+++ src/com/leokom/chess/MainRunner.java
@@ -9,13 +9,13 @@ import org.apache.log4j.Logger;
 public class MainRunner {
 	private static Logger logger = Logger.getLogger(MainRunner.class);
 	public static void main(String[] args) throws IOException {
-		
 		logger.info("Hello");
 		System.out.println( "usermove e2e4" );
 		
 		//TODO: think about buffers
 		BufferedReader r = new BufferedReader(new InputStreamReader( System.in ));
-		
+
+
 		while( true ) {
 			String line = r.readLine();
 			System.out.println( "Line from engine = " + line );
