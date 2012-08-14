4f9285cd6213b5a2aafbfd8e17c613498b00e710
Leonid Rozenblyum <lrozenblyum@gmail.com>
2012-08-14 21:05:08 +0300

    Initial commit of pre-done work

---
+++ src/com/leokom/chess/MainRunner.java
@@ -0,0 +1,24 @@
+package com.leokom.chess;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.apache.log4j.Logger;
+
+public class MainRunner {
+	private static Logger logger = Logger.getLogger(MainRunner.class);
+	public static void main(String[] args) throws IOException {
+		
+		logger.info("Hello");
+		System.out.println( "usermove e2e4" );
+		
+		//TODO: think about buffers
+		BufferedReader r = new BufferedReader(new InputStreamReader( System.in ));
+		
+		while( true ) {
+			String line = r.readLine();
+			System.out.println( "Line from engine = " + line );
+		}
+	}
+}
