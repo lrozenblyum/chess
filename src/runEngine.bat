dce48054342545dace778b83b95bceec2ba92ef0
Leonid Rozenblyum <lrozenblyum@gmail.com>
2012-08-16 22:15:04 +0300

    Added the start-up script for the Winboard which will use our engine

---
+++ src/runEngine.bat
@@ -0,0 +1,7 @@
+SET WINBOARD_PATH=E:\Games\WinBoard-4.6.2\WinBoard\winboard.exe
+rem this directory must be synchronous with jar destination
+rem I use the Winboard installation sub-dir for simplicity
+SET RUNNABLE_JAR_DIRECTORY=E:\Games\WinBoard-4.6.2\LeokomChess
+SET RUN_JAR_PATH=%RUNNABLE_JAR_DIRECTORY%\Chess.jar
+
+%WINBOARD_PATH% -cp -fcp "java -jar %RUN_JAR_PATH%" -fd "%RUNNABLE_JAR_DIRECTORY%"
