ad0045ed051abe1b23e8b61256b7a22bd99054e9
Leonid Rozenblyum <lrozenblyum@gmail.com>
2012-08-16 22:13:48 +0300

    React to the 'quit command' from Winboard engine

--- src/com/leokom/chess/MainRunner.java
+++ src/com/leokom/chess/MainRunner.java
@@ -9,16 +9,23 @@ import org.apache.log4j.Logger;
 public class MainRunner {
 	private static Logger logger = Logger.getLogger(MainRunner.class);
 	public static void main(String[] args) throws IOException {
-		logger.info("Hello");
-		System.out.println( "usermove e2e4" );
-		
+		logger.info( "Starting the chess" );
+
+        System.out.println( "usermove e2e4" );
+
 		//TODO: think about buffers
 		BufferedReader r = new BufferedReader(new InputStreamReader( System.in ));
 
 
 		while( true ) {
+            //TODO: any Thread.sleep needed?
 			String line = r.readLine();
-			System.out.println( "Line from engine = " + line );
+			logger.info( "Line from engine = " + line );
+
+            if ( line != null && line.equals( "quit" ) ) {
+                logger.info( "Received quit command" );
+                break;
+            }
 		}
 	}
 }
