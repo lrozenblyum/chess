ac45eb1b1c777a5df09d4ac70238905b32c6d91f
Leonid Rozenblyum <lrozenblyum@gmail.com>
2012-08-17 20:45:55 +0300

    'Protover' command from engine basic processing

--- src/com/leokom/chess/MainRunner.java
+++ src/com/leokom/chess/MainRunner.java
@@ -42,6 +42,22 @@ public class MainRunner {
                 //this still has no effect
                 System.out.println( "usermove e2e4" );
             }
+
+            //protover N
+            //Beginning in protocol version 2 (in which N=2), this command will be sent immediately after the "xboard" command.
+            //If you receive some other command immediately after "xboard" (such as "new"), you can assume that protocol version 1 is in use.
+            //The "protover" command is the only new command that xboard always sends in version 2. All other new commands to the engine are sent only if the engine first enables them with the "feature" command. Protocol versions will always be simple integers so that they can easily be compared.
+            //Your engine should reply to the protover command by sending the "feature" command (see below) with the list of non-default feature settings that you require, if any.
+            //Your engine should never refuse to run due to receiving a higher protocol version number than it is expecting! New protocol versions will always be compatible with older ones by default; the larger version number is simply a hint that additional "feature" command options added in later protocol versions may be accepted.
+            if ( line.startsWith( "protover" ) ) {
+                //TODO: add analyze if this line is received immediately after xboard
+                //if not - we may assume it's protocol v1
+
+                //TODO: send any 'feature' requests for 2-version if needed
+
+                //TODO: check if 2'nd element exists
+                logger.info( "Protocol version detected = " + line.split( " " )[ 1 ] );
+            }
 		}
 	}
 }
