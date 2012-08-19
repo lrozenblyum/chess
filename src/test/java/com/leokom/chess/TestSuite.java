c1f8df57e1ec71f0851f3e84d84477cdfef776b5
Leonid Rozenblyum <lrozenblyum@gmail.com>
2012-08-19 18:30:19 +0300

    Synchronized packages

---
+++ src/test/java/com/leokom/chess/TestSuite.java
@@ -0,0 +1,15 @@
+package com.leokom.chess;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Author: Leonid
+ * Date-time: 19.08.12 18:10
+ */
+@RunWith( Suite.class )
+@Suite.SuiteClasses({
+    WinBoardTest.class
+})
+public class TestSuite {
+}
---
+++ src/test/java/com/leokom/chess/WinBoardTest.java
@@ -0,0 +1,16 @@
+package com.leokom.chess;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+/**
+ * Author: Leonid
+ * Date-time: 19.08.12 18:16
+ */
+public class WinBoardTest {
+    @Test
+    public void firstTest() {
+        fail( "Checking if JUnit works fine" );
+    }
+}
