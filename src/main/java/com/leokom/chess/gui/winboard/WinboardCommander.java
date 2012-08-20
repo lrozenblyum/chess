866ae4482af974ec96d63f8272deb3a19d0aa72e
Leonid Rozenblyum <lrozenblyum@gmail.com>
2012-08-20 21:32:17 +0300

    [Sonar] Provide Encoding explicitly

--- src/main/java/com/leokom/chess/gui/winboard/WinboardCommander.java
+++ src/main/java/com/leokom/chess/gui/winboard/WinboardCommander.java
@@ -3,18 +3,19 @@ package com.leokom.chess.gui.winboard;
 import com.leokom.chess.gui.Commander;
 import org.apache.log4j.Logger;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
+import java.io.*;
 
 /**
  * Author: Leonid
  * Date-time: 20.08.12 16:13
  */
 class WinboardCommander implements Commander {
-    private BufferedReader reader;
+	/**
+	 * I don't set up UTF-8 since specification says we'll get only latin characters and digits
+	 * US-ASCII is 7-bit latin charset
+	 */
+	private static final String INPUT_ENCODING = "US-ASCII";
+	private BufferedReader reader;
     private PrintStream outputStream;
 	private Logger logger = Logger.getLogger( this.getClass() );
 
@@ -24,8 +25,17 @@ class WinboardCommander implements Commander {
      * @param outputStream
      */
     public WinboardCommander( InputStream inputStream, PrintStream outputStream ) {
-        //TODO: think about buffers, they're not recommended to use
-        this.reader = new BufferedReader( new InputStreamReader( inputStream ) );
+		final InputStreamReader streamReader;
+		try {
+			streamReader = new InputStreamReader( inputStream, INPUT_ENCODING );
+		} catch ( UnsupportedEncodingException e ) {
+			final InstantiationError instantiationError = new InstantiationError(
+				"Java installation seems incorrect. It doesn't support standard encoding = " + INPUT_ENCODING );
+			instantiationError.initCause( e );
+			throw instantiationError;
+		}
+		//TODO: think about buffers, they're not recommended to use
+		this.reader = new BufferedReader( streamReader );
         this.outputStream = outputStream;
     }
 
