18a176926ecc53fbf0e8f46eb8e3935ae766a195
Leonid Rozenblyum <lrozenblyum@gmail.com>
2012-08-20 20:33:58 +0300

    Formatting clean-up

--- src/test/java/com/leokom/chess/gui/WinBoardControllerTest.java
+++ src/test/java/com/leokom/chess/gui/WinBoardControllerTest.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
 /**
  * Author: Leonid
@@ -14,23 +13,23 @@ import static org.junit.Assert.fail;
  */
 public class WinBoardControllerTest {
 
-    //this test should emulate WinBoard behaviour and analyze our reaction on it.
-    //in theory in future we could extract some Winboard emulator
-    @Test
-    public void switchesWinboardToSetUpMode() {
+	//this test should emulate WinBoard behaviour and analyze our reaction on it.
+	//in theory in future we could extract some Winboard emulator
+	@Test
+	public void switchesWinboardToSetUpMode() {
 		//The commander mock is actually EMULATOR OF Winboard behaviour!
 		//TODO: think about Mockito usage?
 		MockCommander commanderMock = new MockCommander();
 
-        WinboardController controller = new WinboardController( commanderMock );
+		WinboardController controller = new WinboardController( commanderMock );
 
 		assertEquals( 1, commanderMock.sentCommands.size() );
 		final String initializationString = "feature done=0";
 		assertEquals( initializationString, commanderMock.sentCommands.get( 0 ) );
-    }
+	}
 
 	private static class MockCommander implements Commander {
-		List< String > sentCommands = new ArrayList< String >();
+		List<String> sentCommands = new ArrayList<String>();
 
 		@Override
 		public void send( String command ) {
