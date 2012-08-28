30ffcc9dc2ad02d86e2f095503b1775a52980b92
Leonid Rozenblyum <lrozenblyum@gmail.com>
2012-08-28 21:15:40 +0300

    Looping tests seperated

--- src/test/java/com/leokom/chess/engine/PositionPawnTest.java
+++ src/test/java/com/leokom/chess/engine/PositionPawnTest.java
@@ -30,37 +30,6 @@ public class PositionPawnTest {
 	}
 
 	/**
-	 * FIDE 3.7b
-	 */
-	@Test
-	public void blackPawn7rank() {
-		//TODO: do we have guarantee characters are ordered alphabetically?
-		for( char file = 'a'; file <= 'h'; file++ ) {
-			Position position = new Position();
-			testPawn( position, file + "7", Side.BLACK, file + "6", file + "5" );
-		}
-	}
-
-	/**
-	 * FIDE 3.7a
-	 */
-	@Test
-	public void blackPawnNotInitialPosition() {
-		for( char file = 'a'; file <= 'h'; file++ ) {
-			//1, 8 aren't possible
-			//2 is source of promotion rules (so we'll test it separately)
-			//7 -> 2 destinations.
-			for ( int rank = 3; rank <= 6; rank++ ) {
-				Position position = new Position();
-				final String sourceRank = String.valueOf( rank );
-				final String expectedRank = String.valueOf( rank - 1 );
-
-				testPawn( position, file + sourceRank, Side.BLACK, file + expectedRank );
-			}
-		}
-	}
-
-	/**
 	 * FIDE 3.7a
 	 */
 	@Test
