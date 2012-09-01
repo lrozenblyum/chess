cab47bd922782a00337b39d9b7a8aa6e244cb490
Leonid Rozenblyum <lrozenblyum@gmail.com>
2012-09-01 21:40:31 +0300

    Red test

--- src/test/java/com/leokom/chess/engine/PositionHasPawnTest.java
+++ src/test/java/com/leokom/chess/engine/PositionHasPawnTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Develop position#hasPawn
@@ -21,5 +22,19 @@ public class PositionHasPawnTest {
 	@Test
 	public void noPawn() {
 		assertFalse( position.hasPawn( "a2" ) );
+	}
+
+	@Test
+	public void presentPawn() {
+		final String square = "g6";
+		position.addPawn( Side.WHITE, square );
+		assertTrue( position.hasPawn( square ) );
+	}
+
+	@Test
+	public void presentPawnInitial() {
+		final String square = "a2";
+		position.addPawn( Side.WHITE, square );
+		assertTrue( position.hasPawn( square ) );
 	}
 }
