6a2253751fb32449f3fc955b04b772af50388c05
Leonid Rozenblyum <lrozenblyum@gmail.com>
2012-09-05 21:20:22 +0300

    Double move test added, which shown need to generalize asserts - and the generalization done

--- src/test/java/com/leokom/chess/engine/PositionPawnNewPositionsTest.java
+++ src/test/java/com/leokom/chess/engine/PositionPawnNewPositionsTest.java
@@ -33,38 +33,54 @@ public class PositionPawnNewPositionsTest {
 	public void singleMove() {
 		final String anySquare = "c3";
 		final Side side = Side.WHITE;
-		position.addPawn( side, anySquare );
 
 		final String squareToMove = "c4";
-		Position newPosition = position.move( anySquare, squareToMove );
 
-		assertHasPawn( newPosition, squareToMove, side );
-		assertEmptySquare( newPosition, anySquare );
+		assertPawnMovement( side, anySquare, squareToMove );
 	}
 
 	@Test
 	public void singleMoveFromInitialPosition() {
 		final String initialSquare = "e2";
 		final Side side = Side.WHITE;
-
-		position.addPawn( side, initialSquare );
-
 		final String squareToMove = "e3";
-		Position newPosition = position.move( initialSquare, squareToMove );
-		assertHasPawn( newPosition, squareToMove, side );
-		assertEmptySquare( newPosition, initialSquare );
+
+		assertPawnMovement( side, initialSquare, squareToMove );
 	}
 
 	@Test
 	public void singleBlackMove() {
 		final String initialSquare = "f4";
 		final Side side = Side.BLACK;
-		position.addPawn( side, initialSquare );
-
 		final String squareToMove = "f3";
+
+		assertPawnMovement( side, initialSquare, squareToMove );
+	}
+
+	@Test
+	public void doubleMove() {
+		final String initialSquare = "c2";
+		final Side side = Side.WHITE;
+		final String squareToMove = "c4";
+
+		assertPawnMovement( side, initialSquare, squareToMove );
+	}
+
+	/**
+	 * Assert that:
+	 * if we add a pawn to the #position
+	 * and call Position#move method,
+	 * the result will have empty initialSquare and pawn on squareToMove
+	 * @param side
+	 * @param initialSquare
+	 * @param squareToMove
+	 */
+	private void assertPawnMovement( Side side, String initialSquare, String squareToMove ) {
+		position.addPawn( side, initialSquare );
 		Position newPosition = position.move( initialSquare, squareToMove );
+
 		assertHasPawn( newPosition, squareToMove, side );
-		assertEmptySquare(newPosition, initialSquare );
+		assertEmptySquare( newPosition, initialSquare );
 	}
 
 	private static void assertEmptySquare( Position position, String square ) {
