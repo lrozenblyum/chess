a5b83708d3cb6717fedc1595cf74cd81abb34409
Leonid Rozenblyum <lrozenblyum@gmail.com>
2012-09-18 21:53:34 +0300

    Left en passant test

---
+++ src/test/java/com/leokom/chess/engine/PositionEnPassantNewTest.java
@@ -0,0 +1,24 @@
+package com.leokom.chess.engine;
+
+import org.junit.Test;
+import static com.leokom.chess.engine.PositionAsserts.*;
+
+/**
+ * Author: Leonid
+ * Date-time: 18.09.12 21:50
+ */
+public class PositionEnPassantNewTest {
+	@Test
+	public void enPassantLeft() {
+		Position position = new Position( "d" );
+		position.addPawn( Side.BLACK, "d5" );
+
+		position.addPawn( Side.WHITE, "e5" );
+
+		Position newPosition = position.move( "e5", "d6" );
+
+		assertEmptySquare( newPosition, "e5" );
+		assertEmptySquare( newPosition, "d5" );
+		assertHasPawn( newPosition, "d6", Side.WHITE );
+	}
+}
