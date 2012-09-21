f72c819cd021d4f3f42e359088b1bdbb68539d6f
Leonid Rozenblyum <lrozenblyum@gmail.com>
2012-09-21 22:32:35 +0300

    Right en passant test for blacks works

--- src/test/java/com/leokom/chess/engine/PositionEnPassantNewTest.java
+++ src/test/java/com/leokom/chess/engine/PositionEnPassantNewTest.java
@@ -2,6 +2,7 @@ package com.leokom.chess.engine;
 
 import org.junit.Test;
 import static com.leokom.chess.engine.PositionAsserts.*;
+import static com.leokom.chess.engine.Side.*;
 
 /**
  * Author: Leonid
@@ -11,28 +12,42 @@ public class PositionEnPassantNewTest {
 	@Test
 	public void enPassantLeft() {
 		Position position = new Position( "d" );
-		position.addPawn( Side.BLACK, "d5" );
+		position.addPawn( BLACK, "d5" );
 
-		position.addPawn( Side.WHITE, "e5" );
+		position.addPawn( WHITE, "e5" );
 
 		Position newPosition = position.move( "e5", "d6" );
 
 		assertEmptySquare( newPosition, "e5" );
-		assertHasPawn( newPosition, "d6", Side.WHITE );
+		assertHasPawn( newPosition, "d6", WHITE );
 		assertEmptySquare( newPosition, "d5" );
 	}
 
 	@Test
 	public void enPassantLeftTriangle() {
 		Position position = new Position( "e" );
-		position.addPawn( Side.BLACK, "e5" );
+		position.addPawn( BLACK, "e5" );
 
-		position.addPawn( Side.WHITE, "f5" );
+		position.addPawn( WHITE, "f5" );
 
 		Position newPosition = position.move( "f5", "e6" );
 
 		assertEmptySquare( newPosition, "f5" );
-		assertHasPawn( newPosition, "e6", Side.WHITE );
+		assertHasPawn( newPosition, "e6", WHITE );
 		assertEmptySquare( newPosition, "e5" );
+	}
+
+	@Test
+	public void right() {
+		Position position = new Position( "g" );
+		position.addPawn( BLACK, "g5" );
+
+		position.addPawn( WHITE, "f5" );
+
+		Position newPosition = position.move( "f5", "g6" );
+
+		assertEmptySquare( newPosition, "f5" );
+		assertEmptySquare( newPosition, "g5" );
+		assertHasPawn( newPosition, "g6", WHITE );
 	}
 }
