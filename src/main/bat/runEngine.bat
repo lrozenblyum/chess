fef274fb1686e1794066738b29ff459fae2ab0cf
Leonid Rozenblyum <lrozenblyum@gmail.com>
2012-11-11 21:09:11 +0200

    Added comment how to enable debug mode of Winboard server.

--- src/main/bat/runEngine.bat
+++ src/main/bat/runEngine.bat
@@ -4,4 +4,6 @@ rem I use the Winboard installation sub-dir for simplicity
 SET RUNNABLE_JAR_DIRECTORY=E:\Games\WinBoard-4.6.2\LeokomChess
 SET RUN_JAR_PATH=%RUNNABLE_JAR_DIRECTORY%\Chess.jar
 
+
 %WINBOARD_PATH% -cp -fcp "java -jar %RUN_JAR_PATH%" -fd "%RUNNABLE_JAR_DIRECTORY%"
+rem for debugging purposes add -debug to the line above
