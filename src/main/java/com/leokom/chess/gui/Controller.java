0bb600fe9f92fd7235ecb08bd079509e384cff80
Leonid Rozenblyum <lrozenblyum@gmail.com>
2012-11-17 21:53:41 +0200

    Described purpose of the controller interface

--- src/main/java/com/leokom/chess/gui/Controller.java
+++ src/main/java/com/leokom/chess/gui/Controller.java
@@ -3,6 +3,9 @@ package com.leokom.chess.gui;
 import com.leokom.chess.gui.Listener;
 
 /**
+ * Engine-agnostic interface.
+ * Responsible to run the main loop, set up event listeners
+ * and send commands to the GUI
  * Author: Leonid
  * Date-time: 20.08.12 20:48
  */
