d56eb077cb7a75b2a2caee73348d4f5850e5f218
Leonid Rozenblyum <lrozenblyum@gmail.com>
2012-11-25 22:39:56 +0200

    Removed irrelevant comment

--- src/main/java/com/leokom/chess/gui/winboard/WinboardCommander.java
+++ src/main/java/com/leokom/chess/gui/winboard/WinboardCommander.java
@@ -5,8 +5,6 @@ import com.leokom.chess.gui.Communicator;
 /**
  * Low-level Winboard-commands for some abstraction
  * and easier testing.
- * However it's hard to judge if it's not intersecting
- * with WinboardPlayer
  * Author: Leonid
  * Date-time: 10.11.12 21:54
  */
