dbcf8eb61046e400d53918308a58e7478b8efc72
Leonid Rozenblyum <lrozenblyum@gmail.com>
2012-11-25 21:08:24 +0200

    Comment.

--- src/main/java/com/leokom/chess/gui/winboard/UserMoveListener.java
+++ src/main/java/com/leokom/chess/gui/winboard/UserMoveListener.java
@@ -5,5 +5,6 @@ package com.leokom.chess.gui.winboard;
  * or 5 when a promotion is involved).
  */
 interface UserMoveListener {
+	//TODO: provide data about the actual user move
 	void execute();
 }
