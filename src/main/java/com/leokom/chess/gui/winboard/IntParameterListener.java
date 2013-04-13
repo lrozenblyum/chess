f7b9aed982358e2bfd4116fcadb860ed0c8487c2
Leonid Rozenblyum <lrozenblyum@gmail.com>
2013-04-13 17:38:23 +0300

    Hid interface.

--- src/main/java/com/leokom/chess/gui/winboard/IntParameterListener.java
+++ src/main/java/com/leokom/chess/gui/winboard/IntParameterListener.java
@@ -4,6 +4,6 @@ package com.leokom.chess.gui.winboard;
  * Author: Leonid
  * Date-time: 31.12.12 17:41
  */
-public interface IntParameterListener {
+interface IntParameterListener {
 	void execute( int paramToPass );
 }
