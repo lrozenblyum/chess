1afc1ea61c0bc219729e0cc71b2d1f1d790a0e5c
Leonid Rozenblyum <lrozenblyum@gmail.com>
2013-04-17 21:33:06 +0300

    Updated docs to current package goals.

--- src/main/java/com/leokom/chess/player/package-info.java
+++ src/main/java/com/leokom/chess/player/package-info.java
@@ -1,5 +1,6 @@
 /**
  * Universal, engine-agnostic Player with all required
  * dependencies.
+ * We could store here sub-packages for different Player implementations
  */
 package com.leokom.chess.player;
