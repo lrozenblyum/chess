b5bdf24a824f9a08460c609d6cab2977c96abbea
Leonid Rozenblyum <lrozenblyum@gmail.com>
2013-07-27 21:53:09 +0300

    Hid interface.

--- src/main/java/com/leokom/chess/player/winboard/ResignListener.java
+++ src/main/java/com/leokom/chess/player/winboard/ResignListener.java
@@ -4,6 +4,6 @@ package com.leokom.chess.player.winboard;
  * Author: Leonid
  * Date-time: 27.07.13 21:48
  */
-public interface ResignListener extends NoParametersListener {
+interface ResignListener extends NoParametersListener {
 	void execute();
 }
