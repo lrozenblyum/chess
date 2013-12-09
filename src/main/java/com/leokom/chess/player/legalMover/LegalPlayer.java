59500cb6d8116a36350449e2d63139aba6eea42d
Leonid Rozenblyum <lrozenblyum@gmail.com>
2013-12-09 22:07:17 +0200

    Created skeleton of the LegalPlayer

---
+++ src/main/java/com/leokom/chess/player/legalMover/LegalPlayer.java
@@ -0,0 +1,39 @@
+package com.leokom.chess.player.legalMover;
+
+import com.leokom.chess.player.Player;
+
+/**
+ * Author: Leonid
+ * Date-time: 09.12.13 22:02
+ */
+public class LegalPlayer implements Player {
+	@Override
+	public void run() {
+
+	}
+
+	@Override
+	public void opponentOfferedDraw() {
+
+	}
+
+	@Override
+	public void opponentAgreedToDrawOffer() {
+
+	}
+
+	@Override
+	public void opponentMoved( String opponentMove ) {
+
+	}
+
+	@Override
+	public void opponentResigned() {
+
+	}
+
+	@Override
+	public void setOpponent( Player opponent ) {
+
+	}
+}
---
+++ src/test/java/com.leokom.chess.player.legalMover/LegalPlayerTest.java
@@ -0,0 +1,15 @@
+package com.leokom.chess.player.legalMover;
+
+import org.junit.Test;
+
+
+/**
+ * Author: Leonid
+ * Date-time: 09.12.13 22:01
+ */
+public class LegalPlayerTest {
+	@Test
+	public void legalPlayerCreation() {
+		new LegalPlayer();
+	}
+}
