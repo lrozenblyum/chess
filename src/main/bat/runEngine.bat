f619d03e65a51670b1a9fdb25e5409e4e6616d5f
Leonid Rozenblyum <lrozenblyum@gmail.com>
2014-01-01 00:36:46 +0200

    Description about debug mode possibility

--- src/main/bat/runEngine.bat
+++ src/main/bat/runEngine.bat
@@ -4,6 +4,7 @@ rem I use the Winboard installation sub-dir for simplicity
 SET RUNNABLE_JAR_DIRECTORY=E:\Games\WinBoard-4.6.2\LeokomChess
 SET RUN_JAR_PATH=%RUNNABLE_JAR_DIRECTORY%\Chess.jar
 
-
+rem to turn on debug mode add -debug
+rem it will create winboard debug log
 %WINBOARD_PATH% -cp -fcp "D:\Java\jdk1.7.0_45\bin\java.exe -jar %RUN_JAR_PATH%" -fd "%RUNNABLE_JAR_DIRECTORY%"
 rem for debugging purposes add -debug to the line above
