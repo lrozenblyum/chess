98309ca4f873bc0303785e869c97b27ef10fb427
Leonid Rozenblyum <lrozenblyum@gmail.com>
2014-05-06 22:22:45 +0300

    System property respect needs to be done

--- src/test/java/com/leokom/chess/MainRunnerTest.java
+++ src/test/java/com/leokom/chess/MainRunnerTest.java
@@ -2,6 +2,7 @@ package com.leokom.chess;
 
 import com.leokom.chess.engine.Side;
 import com.leokom.chess.player.Player;
+import com.leokom.chess.player.legalMover.LegalPlayer;
 import com.leokom.chess.player.simple.SimpleEnginePlayer;
 import org.junit.Test;
 
@@ -12,5 +13,13 @@ public class MainRunnerTest {
 	public void noSystemPropertiesDefaultPlayer() {
 		final Player player = MainRunner.createPlayer( Side.WHITE );
 		assertTrue( player instanceof SimpleEnginePlayer );
+	}
+
+	@Test
+	public void legalSelected() {
+		System.setProperty( "black", "LegalPlayer" );
+
+		final Player player = MainRunner.createPlayer( Side.BLACK );
+		assertTrue( player instanceof LegalPlayer );
 	}
 }
