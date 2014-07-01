0176175587aa8656ce8ce053d57664140731dd57
Leonid Rozenblyum <lrozenblyum@gmail.com>
2014-07-01 22:31:06 +0300

    Java path extracted to a variable

--- src/main/bat/runEngine.bat
+++ src/main/bat/runEngine.bat
@@ -2,10 +2,11 @@ SET WINBOARD_PATH=E:\Games\WinBoard-4.7.3\WinBoard\winboard.exe
 rem this directory must be synchronous with jar destination
 rem I use the Winboard installation sub-dir for simplicity
 SET RUNNABLE_JAR_DIRECTORY=E:\Games\WinBoard-4.7.3\LeokomChess
+SET JAVA_PATH=D:\Java\jdk1.7.0_45\bin\java.exe
 SET RUN_JAR_PATH=%RUNNABLE_JAR_DIRECTORY%\Chess.jar
 SET RUN_OPTIONS=-Dblack=Legal
 
 rem to turn on debug mode add -debug
 rem it will create winboard debug log
-%WINBOARD_PATH% -debug -cp -fcp "D:\Java\jdk1.7.0_45\bin\java.exe %RUN_OPTIONS% -jar %RUN_JAR_PATH%" -fd "%RUNNABLE_JAR_DIRECTORY%"
+%WINBOARD_PATH% -debug -cp -fcp "%JAVA_PATH% %RUN_OPTIONS% -jar %RUN_JAR_PATH%" -fd "%RUNNABLE_JAR_DIRECTORY%"
 rem for debugging purposes add -debug to the line above
