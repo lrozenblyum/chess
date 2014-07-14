e0ed531e28cf31215b1b17db95faa992d25dc799
Leonid Rozenblyum <lrozenblyum@gmail.com>
2014-07-14 23:16:21 +0300

    Preparing center control evaluator #134

---
+++ src/main/java/com/leokom/chess/player/legalMover/CenterControlEvaluator.java
@@ -0,0 +1,19 @@
+package com.leokom.chess.player.legalMover;
+
+import com.leokom.chess.engine.Move;
+import com.leokom.chess.engine.Position;
+
+/**
+ * Author: Leonid
+ * Date-time: 14.07.14 23:11
+ */
+class CenterControlEvaluator implements Evaluator {
+	/**
+	 * @inheritDoc
+	 */
+	@Override
+	public double evaluateMove( Position position, Move move ) {
+		//TODO: implement it
+		return 0;
+	}
+}
--- src/main/java/com/leokom/chess/player/legalMover/LegalPlayer.java
+++ src/main/java/com/leokom/chess/player/legalMover/LegalPlayer.java
@@ -99,7 +99,15 @@ public class LegalPlayer implements Player {
 		//so far no, but from human logic we need that possibility
 		//among 2 'equal' moves we would like to select according to some
 		//compare 1-to-another logic
-		return new CastlingSafetyEvaluator().evaluateMove( position, move );
+		List< Evaluator > evaluators = Arrays.asList( new CastlingSafetyEvaluator(), new CenterControlEvaluator() );
+		double estimate = 0;
+		for ( Evaluator evaluator : evaluators ) {
+			// rather safe if all evaluators keep convention [0 , 1]
+			//here we also can multiply by wight of each evaluator for us
+			estimate += evaluator.evaluateMove( position, move );
+		}
+
+		return estimate;
 	}
 
 	private Move getMoveWithMaxRating( Map< Move, Double > moveValues ) {
