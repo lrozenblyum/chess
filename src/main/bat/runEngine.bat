70f3a3f7adae4c8f16d3d408fbd84dc2b2d06d51
Leonid Rozenblyum <lrozenblyum@gmail.com>
2014-07-14 22:53:52 +0300

    File->New game will cause reloading chess so we don't need to react to signal about the new game so far (while playing Legal=Black) #119

--- src/main/bat/runEngine.bat
+++ src/main/bat/runEngine.bat
@@ -8,5 +8,6 @@ SET RUN_OPTIONS=-Dblack=Legal
 
 rem to turn on debug mode add -debug
 rem it will create winboard debug log
-%WINBOARD_PATH% -debug -cp -fcp "%JAVA_PATH% %RUN_OPTIONS% -jar %RUN_JAR_PATH%" -fd "%RUNNABLE_JAR_DIRECTORY%"
+
+%WINBOARD_PATH% -debug -cp -reuseFirst false -fcp "%JAVA_PATH% %RUN_OPTIONS% -jar %RUN_JAR_PATH%" -fd "%RUNNABLE_JAR_DIRECTORY%"
 rem for debugging purposes add -debug to the line above
