b2daf760f2573e9116594b9599ca07c2f59521f6
Leonid Rozenblyum <lrozenblyum@gmail.com>
2014-07-15 21:44:52 +0300

    Preparing center control evaluator #134 Enriching test infrastructure

--- src/test/java/com/leokom/chess/player/legalMover/CenterControlEvaluatorTest.java
+++ src/test/java/com/leokom/chess/player/legalMover/CenterControlEvaluatorTest.java
@@ -5,33 +5,23 @@ import com.leokom.chess.engine.Position;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class CenterControlEvaluatorTest {
 	private CenterControlEvaluator evaluator;
+	private EvaluatorAsserts asserts;
 
 	@Before
 	public void prepare() {
 		evaluator = new CenterControlEvaluator();
+		asserts = new EvaluatorAsserts( evaluator );
 	}
 
 	@Test
 	public void evaluateMove() {
-	Position position = new Position( null );
+		Position position = new Position( null );
 
 		Move centerControlMove = new Move( null, null );
 		Move notCenterControlMove = new Move( null, null );
 
-		assertBetterMoveDetected( position, centerControlMove, notCenterControlMove );
-	}
-
-	private void assertBetterMoveDetected( Position position, Move expectedBetter, Move expectedWorse ) {
-		double betterMoveEstimate = evaluator.evaluateMove( position, expectedBetter );
-		double worseMoveEstimate = evaluator.evaluateMove( position, expectedWorse );
-
-		assertTrue(
-			String.format( "Better move must have higher rating. Expected better : %s -> %s, expected worse: %s -> %s ",
-				expectedBetter, betterMoveEstimate, expectedWorse, worseMoveEstimate ),
-			betterMoveEstimate > worseMoveEstimate );
+		asserts.assertBetterMoveDetected( position, centerControlMove, notCenterControlMove );
 	}
 }
---
+++ src/test/java/com/leokom/chess/player/legalMover/EvaluatorAsserts.java
@@ -0,0 +1,27 @@
+package com.leokom.chess.player.legalMover;
+
+import com.leokom.chess.engine.Move;
+import com.leokom.chess.engine.Position;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Author: Leonid
+ * Date-time: 15.07.14 21:42
+ */
+public class EvaluatorAsserts {
+	private final Evaluator evaluator;
+
+	public EvaluatorAsserts( Evaluator evaluator ) {
+		this.evaluator = evaluator;
+	}
+	void assertBetterMoveDetected( Position position, Move expectedBetter, Move expectedWorse ) {
+		double betterMoveEstimate = evaluator.evaluateMove( position, expectedBetter );
+		double worseMoveEstimate = evaluator.evaluateMove( position, expectedWorse );
+
+		assertTrue(
+				String.format( "Better move must have higher rating. Expected better : %s -> %s, expected worse: %s -> %s ",
+						expectedBetter, betterMoveEstimate, expectedWorse, worseMoveEstimate ),
+				betterMoveEstimate > worseMoveEstimate );
+	}
+}
