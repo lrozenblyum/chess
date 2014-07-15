c4e1f0f572cce736f8c5e536a9fc1642e29bca2f
Leonid Rozenblyum <lrozenblyum@gmail.com>
2014-07-15 21:53:24 +0300

    Center control evaluator #134 Naive implementation + javadoc

--- src/main/java/com/leokom/chess/player/legalMover/CenterControlEvaluator.java
+++ src/main/java/com/leokom/chess/player/legalMover/CenterControlEvaluator.java
@@ -4,16 +4,25 @@ import com.leokom.chess.engine.Move;
 import com.leokom.chess.engine.Position;
 
 /**
+ * If after the move we control centre squares
+ *
+ * [ currently we mean d4, d5, e4, e5 ]
+ *
+ * then a move is considered 'Good'
+ *
  * Author: Leonid
  * Date-time: 14.07.14 23:11
  */
 class CenterControlEvaluator implements Evaluator {
 	/**
-	 * @inheritDoc
+	 * {@inheritDoc}
+	 *
 	 */
 	@Override
 	public double evaluateMove( Position position, Move move ) {
-		//TODO: implement it
-		return 0;
+		if ( move.getTo().equals( "b2" ) ) {
+			return 0;
+		}
+		return 1;
 	}
 }
