d701f802cbec1fe9f9a3ef98b7f3929f3af0ef77
Leonid Rozenblyum <lrozenblyum@gmail.com>
2014-07-23 22:33:16 +0300

    TODOs

--- src/main/java/com/leokom/chess/player/legalMover/MobilityEvaluator.java
+++ src/main/java/com/leokom/chess/player/legalMover/MobilityEvaluator.java
@@ -12,12 +12,26 @@ import com.leokom.chess.engine.Side;
  * Date-time: 23.07.14 21:46
  */
 public class MobilityEvaluator implements Evaluator {
+
+	//introduced this 'normalizer' to have
+	//result in borders [0 , 1]
+	//TODO: however it looks TOO limiting
+	//more often than not we'll have result e.g. 0.02
+	//and need big multiplier to have serious effect among other evaluators
+	//possible solution:
+	//execute 'normalizing' on caller's side among ACTUAL moves from the position
+	//instead of doing it basing on theoretical information
+
+	//TODO: calculate theoretical max. possible moves count in a position (9 promoted queens + all others? )
+	private static final int MAXIMAL_POSSIBLE_MOVES = 1000;
+
 	@Override
 	public double evaluateMove( Position position, Move move ) {
 		final Position target = position.move( move );
 
 		final Side ourSide = position.getSide( move.getFrom() );
 
-		return target.getMoves( ourSide ).size();
+		final int legalMoves = target.getMoves( ourSide ).size();
+		return (double) legalMoves / MAXIMAL_POSSIBLE_MOVES;
 	}
 }
