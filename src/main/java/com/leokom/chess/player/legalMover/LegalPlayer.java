e3be41edfcd121f43ff032cc31b2be03dd770343
Leonid Rozenblyum <lrozenblyum@gmail.com>
2014-07-23 22:37:34 +0300

    Injecting the mobility evaluator

--- src/main/java/com/leokom/chess/player/legalMover/LegalPlayer.java
+++ src/main/java/com/leokom/chess/player/legalMover/LegalPlayer.java
@@ -99,11 +99,11 @@ public class LegalPlayer implements Player {
 		//so far no, but from human logic we need that possibility
 		//among 2 'equal' moves we would like to select according to some
 		//compare 1-to-another logic
-		List< Evaluator > evaluators = Arrays.asList( new CastlingSafetyEvaluator(), new CenterControlEvaluator() );
+		List< Evaluator > evaluators = Arrays.asList( new CastlingSafetyEvaluator(), new CenterControlEvaluator(), new MobilityEvaluator() );
 		double estimate = 0;
 		for ( Evaluator evaluator : evaluators ) {
 			// rather safe if all evaluators keep convention [0 , 1]
-			//here we also can multiply by wight of each evaluator for us
+			//here we also can multiply by weight of each evaluator for us
 			estimate += evaluator.evaluateMove( position, move );
 		}
 
