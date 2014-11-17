46f882c518c64c48ca8cfdb15c6e598b15ec7ace
Leonid Rozenblyum <lrozenblyum@gmail.com>
2014-11-17 23:17:03 +0200

    Dummy but better implementation of man-in-the-middle

--- src/main/java/com/leokom/chess/engine/validator/Validator.java
+++ src/main/java/com/leokom/chess/engine/validator/Validator.java
@@ -1,6 +1,7 @@
 package com.leokom.chess.engine.validator;
 
 import com.leokom.chess.engine.Move;
+import com.leokom.chess.engine.Side;
 import com.leokom.chess.player.Player;
 
 /**
@@ -20,7 +21,10 @@ public class Validator implements Player {
 	private final Player whitePlayer;
 	private final Player blackPlayer;
 
+	private Side sideToMove = Side.WHITE;
+
 	public Validator( Player white, Player black ) {
+
 		this.whitePlayer = white;
 		this.blackPlayer = black;
 	}
@@ -42,9 +46,17 @@ public class Validator implements Player {
 
 	@Override
 	public void opponentMoved( Move opponentMove ) {
-		//TODO: definitely strange fast implementation
-		blackPlayer.opponentMoved( opponentMove );
-		whitePlayer.opponentMoved( opponentMove );
+		sideToMove = sideToMove.opposite();
+
+		//TODO: a little bit better implementation
+		//but it has issues : when a player goes when he is not allowed
+		//(not his turn) we'll pass it
+		if ( sideToMove == Side.BLACK ) {
+			blackPlayer.opponentMoved( opponentMove );
+		}
+		else {
+			whitePlayer.opponentMoved( opponentMove );
+		}
 	}
 
 	@Override
