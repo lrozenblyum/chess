3561925ab82d46afb82088bd2da63a91b6276eab
Leonid Rozenblyum <lrozenblyum@gmail.com>
2016-02-10 21:44:56 +0200

    Comments about 2-players match and current (unlogical) switchers usage.

--- src/main/bat/runTwoEngines.bat
+++ src/main/bat/runTwoEngines.bat
@@ -6,12 +6,16 @@ SET RUNNABLE_JAR_DIRECTORY_2=E:\Games\WinBoard-4.7.3\LeokomChessTest
 SET JAVA_PATH=D:\Java\jdk1.8.0_25\bin\java.exe
 SET RUN_JAR_PATH=%RUNNABLE_JAR_DIRECTORY%\Chess.jar
 SET RUN_JAR_PATH_2=%RUNNABLE_JAR_DIRECTORY_2%\Chess.jar
+
+rem for Winboard integrations we simply don't care about color of the opponent
+rem the goal is just to specify WHAT is the opponent
+rem specifying 'unlogically' -Dblack=Legal since -Dwhite=Legal is unsupported now
 SET RUN_OPTIONS=-Dblack=Legal
-SET RUN_OPTIONS_2=-Dwhite=Legal -Dblack=Winboard
+SET RUN_OPTIONS_2=-Dblack=Legal
 
 rem to turn on debug mode add -debug
 rem it will create winboard debug log
 
 rem -mg means match game
-%WINBOARD_PATH% -debug  -reuseFirst false -mg 1 -fcp "%JAVA_PATH% %RUN_OPTIONS% -jar %RUN_JAR_PATH%" -fd "%RUNNABLE_JAR_DIRECTORY%" -scp "%JAVA_PATH% %RUN_OPTIONS% -jar %RUN_JAR_PATH_2%" -sd "%RUNNABLE_JAR_DIRECTORY_2%"
+%WINBOARD_PATH% -debug  -reuseFirst false -mg 1 -fcp "%JAVA_PATH% %RUN_OPTIONS% -jar %RUN_JAR_PATH%" -fd "%RUNNABLE_JAR_DIRECTORY%" -scp "%JAVA_PATH% %RUN_OPTIONS_2% -jar %RUN_JAR_PATH_2%" -sd "%RUNNABLE_JAR_DIRECTORY_2%"
 
