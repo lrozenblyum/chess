9ad4c218af38862721e2042384d41bd1a2e7d191
Leonid Rozenblyum <lrozenblyum@gmail.com>
2016-04-06 21:23:57 +0300

    Initial simulator behaviour.

--- src/test/java/com/leokom/chess/player/legalMover/Simulator.java
+++ src/test/java/com/leokom/chess/player/legalMover/Simulator.java
@@ -1,5 +1,8 @@
 package com.leokom.chess.player.legalMover;
 
+import com.leokom.chess.Game;
+import com.leokom.chess.player.Player;
+
 /**
  * Enable matches between players
  * Check evaluators to find out their strengths
@@ -10,5 +13,16 @@ package com.leokom.chess.player.legalMover;
  * Author: Leonid
  * Date-time: 06.04.16 21:08
  */
-public class Simulator {
+class Simulator {
+	private final Player first;
+	private final Player second;
+
+	Simulator( Player first, Player second ) {
+		this.first = first;
+		this.second = second;
+	}
+
+	void run() {
+		new Game( this.first, this.second ).run();
+	}
 }
--- src/test/java/com/leokom/chess/player/legalMover/SimulatorTest.java
+++ src/test/java/com/leokom/chess/player/legalMover/SimulatorTest.java
@@ -1,11 +1,27 @@
 package com.leokom.chess.player.legalMover;
 
+import com.leokom.chess.player.Player;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 /**
  * Author: Leonid
  * Date-time: 06.04.16 21:08
  */
 public class SimulatorTest {
+	@Test
+	public void runGames() {
+		final Player first = mock( Player.class );
+		final Player second = mock( Player.class );
+		new Simulator( first, second ).run();
+
+		verify( second ).opponentSuggestsMeStartNewGameBlack();
+		verify( first ).opponentSuggestsMeStartNewGameWhite();
+	}
+
+
 
 }
