1ed9afc4e3f0b380f3d7a2bd58a4a6efbfb26f73
Leonid Rozenblyum <lrozenblyum@gmail.com>
2016-04-06 21:19:35 +0300

    Prepared infrastructure for Simulator

---
+++ src/test/java/com/leokom/chess/player/legalMover/Simulator.java
@@ -0,0 +1,14 @@
+package com.leokom.chess.player.legalMover;
+
+/**
+ * Enable matches between players
+ * Check evaluators to find out their strengths
+ *
+ * Put into test scope to avoid including into Chess jar
+ * Maybe it's worth separate project
+ *
+ * Author: Leonid
+ * Date-time: 06.04.16 21:08
+ */
+public class Simulator {
+}
---
+++ src/test/java/com/leokom/chess/player/legalMover/SimulatorTest.java
@@ -0,0 +1,11 @@
+package com.leokom.chess.player.legalMover;
+
+import static org.junit.Assert.*;
+
+/**
+ * Author: Leonid
+ * Date-time: 06.04.16 21:08
+ */
+public class SimulatorTest {
+
+}
