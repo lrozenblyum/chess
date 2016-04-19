46ef14ebe77fdcf11337af41c0af7ea0b37f0607
Leonid Rozenblyum <lrozenblyum@gmail.com>
2016-04-19 22:54:12 +0300

    Introduced an indirection (BasicEvaluator enum) that should help us inject custom weights for specific evaluators.

---
+++ src/main/java/com/leokom/chess/player/legalMover/BasicEvaluator.java
@@ -0,0 +1,31 @@
+package com.leokom.chess.player.legalMover;
+
+/**
+ * Represent known way to analyze a position.
+ * Every instance of it is some part of our brain we'd like to use to analyze a particular
+ * position move.
+ * Author: Leonid
+ * Date-time: 19.04.16 22:46
+ */
+enum BasicEvaluator {
+//we keep references to instances of evaluators here
+	//so they're practically singletons
+	//any valid Evaluator must be stateless and thread-safe
+	CHECKMATE( new CheckmateEvaluator() ),
+	CASTLING_SAFETY( new CastlingSafetyEvaluator() ),
+	CENTER_CONTROL( new CenterControlEvaluator() ),
+	MATERIAL( new MaterialEvaluator() ),
+	MOBILITY( new MobilityEvaluator() ),
+	PROTECTION( new ProtectionEvaluator() ),
+	SPECIAL_MOVE( new SpecialMoveEvaluator() );
+
+	private final Evaluator evaluator;
+
+	Evaluator getEvaluator() {
+		return evaluator;
+	}
+
+	BasicEvaluator( Evaluator evaluator ) {
+		this.evaluator = evaluator;
+	}
+}
--- src/main/java/com/leokom/chess/player/legalMover/MasterEvaluator.java
+++ src/main/java/com/leokom/chess/player/legalMover/MasterEvaluator.java
@@ -6,6 +6,8 @@ import com.leokom.chess.engine.Position;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.leokom.chess.player.legalMover.BasicEvaluator.*;
+
 /**
  * Central evaluator of a move ('brains', 'decision maker')
  */
@@ -16,28 +18,28 @@ class MasterEvaluator implements Evaluator {
 	//among 2 'equal' moves we would like to select according to some
 	//compare 1-to-another logic
 
-	private Map< Evaluator, Double > evaluatorWeights = new HashMap<>();
+	private Map< BasicEvaluator, Double > evaluatorWeights = new HashMap<>();
 
 	private static final double HIGHEST_PRIORITY = 100.0;
 	private static final double NORMAL_PRIORITY = 1.0;
 	private static final double DISABLED = 0.0;
 
 	{
-		evaluatorWeights.put( new CheckmateEvaluator(), HIGHEST_PRIORITY );
-		evaluatorWeights.put( new CastlingSafetyEvaluator(), NORMAL_PRIORITY );
-		evaluatorWeights.put( new CenterControlEvaluator(), NORMAL_PRIORITY );
-		evaluatorWeights.put( new MobilityEvaluator(), NORMAL_PRIORITY );
-		evaluatorWeights.put( new MaterialEvaluator(), NORMAL_PRIORITY );
-		evaluatorWeights.put( new ProtectionEvaluator(), NORMAL_PRIORITY );
+		evaluatorWeights.put( CHECKMATE, HIGHEST_PRIORITY );
+		evaluatorWeights.put( CASTLING_SAFETY, NORMAL_PRIORITY );
+		evaluatorWeights.put( CENTER_CONTROL, NORMAL_PRIORITY );
+		evaluatorWeights.put( MOBILITY, NORMAL_PRIORITY );
+		evaluatorWeights.put( MATERIAL, NORMAL_PRIORITY );
+		evaluatorWeights.put( PROTECTION, NORMAL_PRIORITY );
 		//this disabling is not absolute. Those moves anyway have chance
 		//e.g. if LegalPlayer selects moves in reverse order (from worse to best)
-		evaluatorWeights.put( new SpecialMoveEvaluator(), DISABLED );
+		evaluatorWeights.put( SPECIAL_MOVE, DISABLED );
 	}
 
 	@Override
 	public double evaluateMove( Position position, Move move ) {
 		return evaluatorWeights.entrySet().stream().mapToDouble( evaluatorEntry -> {
-			final Evaluator evaluator = evaluatorEntry.getKey();
+			final Evaluator evaluator = evaluatorEntry.getKey().getEvaluator();
 			final double weight = evaluatorEntry.getValue();
 			return weight * evaluator.evaluateMove( position, move );
 		} ).sum();
