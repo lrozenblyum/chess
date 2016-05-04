b9501791b9889c68c92e07241eba3fc54f6da9a1
Leonid Rozenblyum <lrozenblyum@gmail.com>
2016-05-04 23:26:32 +0300

    First test to run multiple games per multiple coefficients - to check what influence does the coefficient have. Checking protecting factor.

---
+++ src/test/java/com/leokom/chess/SimulatorMultiDeltaTest.java
@@ -0,0 +1,53 @@
+package com.leokom.chess;
+
+import com.leokom.chess.player.legalMover.EvaluatorType;
+import com.leokom.chess.player.legalMover.LegalPlayer;
+import com.leokom.chess.player.legalMover.MasterEvaluatorBuilder;
+import org.apache.logging.log4j.LogManager;
+import org.junit.Test;
+
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+
+
+/**
+ * Engine to run multiple games with some delta
+ *
+ * Author: Leonid
+ * Date-time: 04.05.16 21:29
+ */
+public class SimulatorMultiDeltaTest {
+
+	/*
+	 * Protection property.
+	 * Coefficient :: [ 0 .. 90 ] delta 10 ==> 10 coefficients probed
+	 * Each probe :: 10 games (5 pairs of games with colours switched)
+	 *
+	 */
+	@Test
+	public void protectionDeltas() {
+		Map< Integer, SimulatorStatistics > statisticsMap = new TreeMap<>();
+
+		for ( int coefficient = 0; coefficient <= 90; coefficient += 10 ) {
+			final LegalPlayer protectionBasedPlayer = new LegalPlayer( new MasterEvaluatorBuilder().weight( EvaluatorType.PROTECTION, coefficient ).build() );
+			final LegalPlayer classicPlayer = new LegalPlayer();
+
+			final SimulatorStatistics stats = new Simulator( protectionBasedPlayer, classicPlayer ).gamePairs( 5 ).run();
+			statisticsMap.put( coefficient, stats );
+		}
+
+		printResults( statisticsMap );
+	}
+
+	private void printResults( Map<Integer, SimulatorStatistics> statisticsMap ) {
+		final String statsPrettyPrinted = statisticsMap.entrySet().stream()
+				.map( entry -> entry.getKey() + " ==> " + entry.getValue() )
+				.collect( Collectors.joining( "\n" ) );
+
+
+		LogManager.getLogger().info( "STATISTICS ::: {}", statsPrettyPrinted );
+	}
+
+
+}
