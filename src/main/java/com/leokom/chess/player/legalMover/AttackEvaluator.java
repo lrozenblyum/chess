5795d9cf6752512d13ee93bb8a1fd6310a83a3e4
Leonid Rozenblyum <lrozenblyum@gmail.com>
2016-08-25 20:57:40 +0300

    WIP: Attack Evaluator.

---
+++ src/main/java/com/leokom/chess/player/legalMover/AttackEvaluator.java
@@ -0,0 +1,15 @@
+package com.leokom.chess.player.legalMover;
+
+import com.leokom.chess.engine.Move;
+import com.leokom.chess.engine.Position;
+
+/**
+ * Author: Leonid
+ * Date-time: 25.08.16 20:54
+ */
+class AttackEvaluator implements Evaluator {
+	@Override
+	public double evaluateMove( Position position, Move move ) {
+		return 0;
+	}
+}
