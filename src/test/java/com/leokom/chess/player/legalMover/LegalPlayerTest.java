lrozenblyum/chess

commit 17108773c31c3666548f572698e33e9a9c8c6f73

Removed king from formulas in Material evaluator. It had too big value and caused the distribution of the evaluator to be not nice (rather different situations were estimated TOO closely). Refactored to reduce duplication.

src/main/java/com/leokom/chess/player/legalMover/MaterialEvaluator.java
@@ -27,16 +27,24 @@ class MaterialEvaluator implements Evaluator {
 
 		Side ourSide = position.getSideToMove();
 
-		Stream< Piece > ourPieces = target.getPieces( ourSide );
-		Stream< Piece > opponentPieces = target.getPieces( ourSide.opposite() );
-
-		int ourMaterialValue = value( ourPieces );
-		int opponentMaterialValue = value( opponentPieces );
+		int ourMaterialValue = getMaterialValue( target, ourSide );
+		int opponentMaterialValue = getMaterialValue( target, ourSide.opposite() );
 
 		int materialAdvantage = ourMaterialValue - opponentMaterialValue;
 		return normalizeAdvantage( materialAdvantage );
 	}
 
+	private int getMaterialValue( Position position, Side side ) {
+		return value( position.getPieces( side ).
+				filter( this::isNotAKing ) );
+
+	}
+
+	private boolean isNotAKing( Piece piece ) {
+		return piece.getPieceType() != PieceType.KING;
+	}
+
+	//king is invaluable
 	private static final Map< PieceType,Integer > VALUES = new
 			HashMap<>();
 	//heuristic, may be dynamic depending on situation on the board!
@@ -46,9 +54,6 @@ class MaterialEvaluator implements Evaluator {
 		VALUES.put( PieceType.BISHOP, 3 );
 		VALUES.put( PieceType.ROOK, 5 );
 		VALUES.put( PieceType.QUEEN, 9 );
-		//TODO: keeping king here makes this evaluator always generating rather good values
-		//since the king is here till the end of game. Probably we should remove it
-		VALUES.put( PieceType.KING, 1000 );
 	}
 
 	static int getValue( PieceType pieceType ) {
@@ -61,7 +66,6 @@ class MaterialEvaluator implements Evaluator {
 
 	//no pawns included here since they are 'promoted'
 	static final int MAXIMAL_VALUE =
-			VALUES.get( PieceType.KING ) +
 					9 * VALUES.get( PieceType.QUEEN ) +
 					2 * VALUES.get( PieceType.ROOK ) +
 					2 * VALUES.get( PieceType.BISHOP ) +

commit 611ba41dada45c017d64f0b09bee5465372fe382

Red test for bug introduced in 17108773c31c3666548f572698e33e9a9c8c6f73

src/test/java/com/leokom/chess/player/legalMover/LegalPlayerTest.java
@@ -102,6 +102,23 @@ public class LegalPlayerTest {
 	}
 
 	@Test
+	public void noProblemsWhenNextMoveGivesPossibilityToCheck() {
+		LegalPlayer player = getLegalPlayer();
+		player.setOpponent( opponent );
+
+		Position position = new Position( Side.BLACK );
+		position.add( Side.BLACK, "h7", PieceType.KING );
+		position.add( Side.WHITE, "g5", PieceType.KING );
+		position.add( Side.WHITE, "a2", PieceType.QUEEN );
+
+		player.setPosition( position, Side.WHITE );
+
+		player.opponentMoved( new Move( "h7", "h8" ) );
+		//during next move it will be possible to check via a2-a1
+		verify( opponent ).opponentMoved( any() );
+	}
+
+	@Test
 	public void legalPlayerCanMoveFirstAfterRun() {
 		LegalPlayer player = getLegalPlayer();
 		player.setOpponent( opponent );