8090d5748f4e47114cdf12e84e9350c75b404007
Leonid Rozenblyum <lrozenblyum@gmail.com>
2016-08-27 21:11:59 +0300

    Removed evaluators implementations that are the same as in denormalized package. Delegating to denormalized on factory level.

--- src/main/java/com/leokom/chess/player/legal/evaluator/denormalized/SpecialMoveEvaluator.java
+++ src/main/java/com/leokom/chess/player/legal/evaluator/denormalized/SpecialMoveEvaluator.java
@@ -17,6 +17,10 @@ class SpecialMoveEvaluator implements Evaluator {
 	private static final int PREFERRED_MOVE = 1;
 	private static final int USUAL_MOVE = 0;
 
+	/**
+	 * {@inheritDoc}
+	 * @return 0 or 1
+	 */
 	@Override
 	public double evaluateMove( Position position, Move move ) {
 		return move == Move.OFFER_DRAW ? PREFERRED_MOVE : USUAL_MOVE;
--- src/main/java/com/leokom/chess/player/legal/evaluator/normalized/NormalizedEvaluatorFactory.java
+++ src/main/java/com/leokom/chess/player/legal/evaluator/normalized/NormalizedEvaluatorFactory.java
@@ -4,6 +4,7 @@ import com.google.common.collect.Maps;
 import com.leokom.chess.player.legal.evaluator.common.Evaluator;
 import com.leokom.chess.player.legal.evaluator.common.EvaluatorFactory;
 import com.leokom.chess.player.legal.evaluator.common.EvaluatorType;
+import com.leokom.chess.player.legal.evaluator.denormalized.DenormalizedEvaluatorFactory;
 
 import java.util.EnumMap;
 import java.util.Map;
@@ -14,6 +15,8 @@ import java.util.Map;
  */
 public class NormalizedEvaluatorFactory implements EvaluatorFactory {
 	private static final Map<EvaluatorType, Evaluator> evaluators;
+	private static final DenormalizedEvaluatorFactory DENORMALIZED_EVALUATOR_FACTORY = new DenormalizedEvaluatorFactory();
+
 	static {
 		//we keep references to instances of evaluators here
 		//so they're practically singletons
@@ -21,19 +24,23 @@ public class NormalizedEvaluatorFactory implements EvaluatorFactory {
 
 		Map< EvaluatorType, Evaluator > evaluatorsMutable = new EnumMap<>( EvaluatorType.class );
 		evaluatorsMutable.put( EvaluatorType.ATTACK, new AttackEvaluator() );
-		evaluatorsMutable.put( EvaluatorType.CASTLING_SAFETY, new CastlingSafetyEvaluator() );
 		evaluatorsMutable.put( EvaluatorType.CENTER_CONTROL, new CenterControlEvaluator() );
-		evaluatorsMutable.put( EvaluatorType.CHECKMATE, new CheckmateEvaluator() );
 		evaluatorsMutable.put( EvaluatorType.MATERIAL, new MaterialEvaluator() );
 		evaluatorsMutable.put( EvaluatorType.MOBILITY, new MobilityEvaluator() );
 		evaluatorsMutable.put( EvaluatorType.PROTECTION, new ProtectionEvaluator() );
-		evaluatorsMutable.put( EvaluatorType.SPECIAL_MOVE, new SpecialMoveEvaluator() );
 
 		evaluators = Maps.immutableEnumMap( evaluatorsMutable );
 	}
 
+	/**
+	 * @implNote part of evaluators might be reused from denormalized package if they already
+	 * provide the correct values
+	 * @param type type of evaluator to get evaluator from
+	 * @return evaluator that is normalized [ 0, 1 ]
+	 */
 	@Override
 	public Evaluator get( EvaluatorType type ) {
-		return evaluators.get( type );
+		return evaluators.get( type ) != null ? evaluators.get( type ) :
+			DENORMALIZED_EVALUATOR_FACTORY.get( type );
 	}
 }
