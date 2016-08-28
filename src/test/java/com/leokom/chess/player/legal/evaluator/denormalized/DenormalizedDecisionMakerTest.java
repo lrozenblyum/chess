9907315acf3fdefedd8d053399febcf3fb3e6c57
Leonid Rozenblyum <lrozenblyum@gmail.com>
2016-08-28 17:07:59 +0300

    Red test to implement some 'smart' algorithm.

--- src/test/java/com/leokom/chess/player/legal/evaluator/denormalized/DenormalizedDecisionMakerTest.java
+++ src/test/java/com/leokom/chess/player/legal/evaluator/denormalized/DenormalizedDecisionMakerTest.java
@@ -1,11 +1,14 @@
 package com.leokom.chess.player.legal.evaluator.denormalized;
 
-import com.leokom.chess.engine.Position;
+import com.leokom.chess.engine.*;
 import com.leokom.chess.player.legal.evaluator.common.DecisionMaker;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Author: Leonid
@@ -24,4 +27,24 @@ public class DenormalizedDecisionMakerTest {
 		assertTrue( decisionMaker.findBestMove( Position.getInitialPosition() ).isPresent() );
 	}
 
+	/**
+	 * Avoid really 'stupid' moves
+	 */
+	@Test
+	public void beSmartALittle() {
+		PositionBuilder position =
+			new PositionBuilder()
+				.add( Side.WHITE, "a2", PieceType.PAWN )
+				.add( Side.WHITE, "b2", PieceType.PAWN )
+				.add( Side.WHITE, "a1", PieceType.QUEEN )
+				.add( Side.BLACK, "c1", PieceType.ROOK )
+				.setSide( Side.WHITE );
+
+		//otherwise we'll lose the queen
+		Move captureIsSmartest = new Move( "a1", "c1" );
+
+		final Optional< Move > moveFound = decisionMaker.findBestMove( position.build() );
+		assertTrue( moveFound.isPresent() );
+		assertEquals( captureIsSmartest, moveFound.get() );
+	}
 }
