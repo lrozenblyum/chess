a69a32720583097c81402af07de4243d542d5bb4
Leonid Rozenblyum <lrozenblyum@gmail.com>
2016-08-28 17:36:20 +0300

    Normalized table generation.

--- src/main/java/com/leokom/chess/player/legal/evaluator/denormalized/DenormalizedDecisionMaker.java
+++ src/main/java/com/leokom/chess/player/legal/evaluator/denormalized/DenormalizedDecisionMaker.java
@@ -19,8 +19,9 @@ import java.util.*;
  * Author: Leonid
  * Date-time: 27.08.16 21:51
  */
-public class DenormalizedDecisionMaker implements DecisionMaker {
+class DenormalizedDecisionMaker implements DecisionMaker {
 	private static final Logger LOG = LogManager.getLogger();
+	private static final double DEFAULT_FOR_EQUAL_NOT_IN_RANGE = 0.5;
 
 	private EvaluatorFactory evaluatorFactory = new DenormalizedEvaluatorFactory();
 
@@ -31,7 +32,23 @@ public class DenormalizedDecisionMaker implements DecisionMaker {
 		if ( legalMoves.isEmpty() ) {
 			return Optional.empty();
 		}
+		Table<EvaluatorType, Move, Double> movesTable = generateInitialTable( position, legalMoves );
 
+
+		movesTable.cellSet().forEach( cell ->
+			LOG.debug( "INITIAL: {} [{}] : {}", cell.getColumnKey(), cell.getRowKey(), cell.getValue() )
+		);
+
+		Table<EvaluatorType, Move, Double> normalizedTable = generateNormalized( movesTable );
+
+		normalizedTable.cellSet().forEach( cell ->
+			LOG.debug( "NORMALIZED: {} [{}] : {}", cell.getColumnKey(), cell.getRowKey(), cell.getValue() )
+		);
+
+		return Optional.of( legalMoves.iterator().next() );
+	}
+
+	private Table<EvaluatorType, Move, Double> generateInitialTable( Position position, Set<Move> legalMoves ) {
 		//TODO: feel free to select more optimal implementation after profiling,
 		//it's the key component of our data structures here
 		Table< EvaluatorType, Move, Double > movesTable = HashBasedTable.create();
@@ -41,11 +58,55 @@ public class DenormalizedDecisionMaker implements DecisionMaker {
 			final Evaluator evaluator = evaluatorFactory.get( evaluatorType );
 			legalMoves.forEach( move -> movesTable.put( evaluatorType, move, evaluator.evaluateMove( position, move ) ) );
 		} );
+		return movesTable;
+	}
 
-		movesTable.cellSet().forEach( cell ->
-			LOG.debug( "{} [{}] : {}", cell.getColumnKey(), cell.getRowKey(), cell.getValue() )
+	private Table<EvaluatorType, Move, Double> generateNormalized( Table<EvaluatorType, Move, Double> movesTable ) {
+		Table< EvaluatorType, Move, Double > normalizedTable = HashBasedTable.create();
+
+		movesTable.rowKeySet().forEach(
+			evaluatorType -> {
+				final Collection<Double> values = movesTable.row( evaluatorType ).values();
+				double maxValue = Collections.max( values );
+				double minValue = Collections.min( values );
+
+				Formula formula;
+				if ( maxValue == minValue ) {
+					if ( maxValue >= 0.0 && maxValue <= 1.0 ) {
+						//ok, all equal but already in normalized range, don't touch?
+						formula = this::identityFormula;
+					}
+					else {
+						formula = this::constantMiddleFormula;
+					}
+				}
+				else {
+					formula = this::standardizedValueFormula;
+				}
+
+				movesTable.row( evaluatorType ).
+						forEach( (move, value) ->
+								normalizedTable.put( evaluatorType, move, formula.accept( maxValue, minValue, value ) ) );
+			}
 		);
+		return normalizedTable;
+	}
 
-		return Optional.of( legalMoves.iterator().next() );
+	private double identityFormula( double maxValue, double minValue, Double value ) {
+		return value;
+	}
+
+
+	private double constantMiddleFormula( double maxValue, double minValue, Double value ) {
+		return DEFAULT_FOR_EQUAL_NOT_IN_RANGE;
+	}
+
+	private double standardizedValueFormula( double maxValue, double minValue, Double value ) {
+		return ( value - minValue ) / ( maxValue - minValue );
+	}
+
+	@FunctionalInterface
+	private interface Formula {
+		double accept( double maxValue, double minValue, Double value );
 	}
 }
