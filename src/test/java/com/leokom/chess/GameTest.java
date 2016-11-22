a28fff80408525e91953c7f3081296c195a25a3c
Leonid Rozenblyum <lrozenblyum@gmail.com>
2016-11-22 22:23:00 +0200

    Extended basic test coverage thx to pitest mutants report.

--- src/test/java/com/leokom/chess/GameTest.java
+++ src/test/java/com/leokom/chess/GameTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 /**
@@ -16,11 +17,27 @@ public class GameTest {
     @Test
     public void gameCanBeFinishedWithoutStart() {
         Player white = mock( Player.class );
-        when( white.getPosition() ).thenReturn( Position.getInitialPosition() );
+        setPosition(white);
 
         Player black = mock( Player.class );
         Player winner = new Game( white, black ).run();
         //no exceptions expected
         assertNull( winner );
+    }
+
+    @Test
+    public void verifyOpponentSetting() {
+        Player white = mock( Player.class );
+        setPosition( white );
+        Player black = mock( Player.class );
+
+        new Game( white, black ).run();
+
+        verify( white ).setOpponent( black );
+        verify( black ).setOpponent( white );
+    }
+
+    private void setPosition(Player white) {
+        when( white.getPosition() ).thenReturn( Position.getInitialPosition() );
     }
 }
