c105edc5aa9d7b072dfb24d8be072a4a10127454
Leonid Rozenblyum <lrozenblyum@gmail.com>
2016-12-11 21:48:27 +0200

    Generalized 2 bat files via the 3'd one. Another way would be a properties file.

--- src/main/bat/runTwoEngines.bat
+++ src/main/bat/runTwoEngines.bat
@@ -1,16 +1,10 @@
-SET WINBOARD_PATH=E:\Games\WinBoard-4.7.3\WinBoard\winboard.exe
-rem this directory must be synchronous with jar destination
-rem I use the Winboard installation sub-dir for simplicity
-SET RUNNABLE_JAR_DIRECTORY=E:\Games\WinBoard-4.7.3\LeokomChess
+call variables.bat
 SET RUNNABLE_JAR_DIRECTORY_2=E:\Games\WinBoard-4.7.3\LeokomChessTest
-SET JAVA_PATH=D:\Java\jdk1.8.0_77\bin\java.exe
-SET RUN_JAR_PATH=%RUNNABLE_JAR_DIRECTORY%\Chess.jar
 SET RUN_JAR_PATH_2=%RUNNABLE_JAR_DIRECTORY_2%\Chess.jar
 
 rem for Winboard integrations we simply don't care about color of the opponent
 rem the goal is just to specify WHAT is the opponent
 rem specifying 'unlogically' -Dblack=Legal since -Dwhite=Legal is unsupported now
-SET RUN_OPTIONS=-Dblack=Legal
 SET RUN_OPTIONS_2=-Dblack=Legal
 
 rem to turn on debug mode add -debug
---
+++ src/main/bat/variables.bat
@@ -0,0 +1,7 @@
+SET WINBOARD_PATH=E:\Games\WinBoard-4.7.3\WinBoard\winboard.exe
+rem this directory must be synchronous with jar destination
+rem I use the Winboard installation sub-dir for simplicity
+SET RUNNABLE_JAR_DIRECTORY=E:\Games\WinBoard-4.7.3\LeokomChess
+SET JAVA_PATH=D:\Java\jdk1.8.0_77\bin\java.exe
+SET RUN_JAR_PATH=%RUNNABLE_JAR_DIRECTORY%\Chess.jar
+SET RUN_OPTIONS=-Dblack=Legal
