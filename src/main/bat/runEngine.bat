fba23cd41d83b6f024311ee693d34d85a0e13079
Leonid Rozenblyum <lrozenblyum@gmail.com>
2016-12-17 19:54:00 +0200

    Refactoring : switched to multi-line command to run, thus easier to maintain / review changes and making duplication more obvious.

--- src/main/bat/runEngine.bat
+++ src/main/bat/runEngine.bat
@@ -3,4 +3,9 @@ call variables.bat
 rem to turn on debug mode add -debug
 rem it will create winboard debug log
 
-%WINBOARD_PATH% -debug -cp -reuseFirst false -fcp "%ENGINE%"  -fd "%RUNNABLE_JAR_DIRECTORY%"
+%WINBOARD_PATH% ^
+-debug ^
+-cp ^
+-reuseFirst false ^
+-fcp "%ENGINE%" ^
+-fd "%RUNNABLE_JAR_DIRECTORY%"
--- src/main/bat/runTwoEngines.bat
+++ src/main/bat/runTwoEngines.bat
@@ -14,5 +14,12 @@ rem to turn on debug mode add -debug
 rem it will create winboard debug log
 
 rem -mg means match game
-%WINBOARD_PATH% -debug  -reuseFirst false -mg %MATCHES_COUNT% -fcp "%ENGINE%" -fd "%RUNNABLE_JAR_DIRECTORY%" -scp "%ENGINE_2%" -sd "%RUNNABLE_JAR_DIRECTORY_2%"
+%WINBOARD_PATH% ^
+-debug ^
+-reuseFirst false ^
+-mg %MATCHES_COUNT% ^
+-fcp "%ENGINE%" ^
+-fd "%RUNNABLE_JAR_DIRECTORY%" ^
+-scp "%ENGINE_2%" ^
+-sd "%RUNNABLE_JAR_DIRECTORY_2%"
 
