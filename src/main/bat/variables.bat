e3d7b29a0807f7294cbc9c1d95d4ab11b842ec6a
Leonid Rozenblyum <lrozenblyum@gmail.com>
2017-01-09 21:51:54 +0200

    Improved documentation about the ways to run Chess.

--- src/main/bat/variables.bat
+++ src/main/bat/variables.bat
@@ -2,6 +2,7 @@ SET WINBOARD_INSTALLATION_PATH=E:\Games\WinBoard-4.7.3
 rem UI that we use to run our Chess with
 SET WINBOARD_PATH=%WINBOARD_INSTALLATION_PATH%\WinBoard\winboard.exe
 rem I use the Winboard installation as a Chess deployment target
+rem it should be equal to 'project.deployDirectory' property in pom.xml
 SET RUNNABLE_JAR_DIRECTORY=%WINBOARD_INSTALLATION_PATH%\LeokomChess
 SET JAVA_PATH=D:\Java\jdk1.8.0_77\bin\java.exe
 SET RUN_JAR_PATH=%RUNNABLE_JAR_DIRECTORY%\Chess.jar
