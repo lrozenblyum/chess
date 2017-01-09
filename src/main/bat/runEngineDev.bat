08d87ebc354f4e02608990fd0da29e71442ad7e3
Leonid Rozenblyum <lrozenblyum@gmail.com>
2017-01-09 21:31:36 +0200

    Dev mode runner implemented.

---
+++ src/main/bat/runEngineDev.bat
@@ -0,0 +1,25 @@
+rem =======================================================
+rem Runs Chess in development mode
+rem skips tests, packages fast to correct target directory
+rem =======================================================
+
+rem ----
+rem Technical remarks:
+rem current solution is based on simple Maven and other bat-file usage
+rem Other technical possibilities to implement the job is
+rem a) call java directly from the bat file:
+rem        * need to form correct classpath or duplicate it with maven's
+rem b) call mvn exec:java
+rem        * here I didn't find a way to make winboard use the mvn's console
+rem ----
+
+set INITIAL_DIRECTORY=%cd%
+
+rem based on fact we're in src/main/bat and need to go to pom.xml location
+cd ..\..\..
+rem need to use call, otherwise maven takes the control over the batch
+call %M3_HOME%bin\mvn package -DskipTests=true
+
+rem another option would be cd src/main/bat
+cd %INITIAL_DIRECTORY%
+call runEngine.bat
