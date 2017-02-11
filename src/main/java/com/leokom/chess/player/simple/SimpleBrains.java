619e690b133f2f3472b011b1229c6ddbe07b58ea
Leonid Rozenblyum <lrozenblyum@gmail.com>
2017-02-11 21:35:49 +0200

    Simple yet working implementation of Simple brains. Just 4 tests are left red.

--- src/main/java/com/leokom/chess/player/simple/SimpleBrains.java
+++ src/main/java/com/leokom/chess/player/simple/SimpleBrains.java
@@ -4,7 +4,7 @@ import com.leokom.chess.engine.Move;
 import com.leokom.chess.engine.Position;
 import com.leokom.chess.player.legal.evaluator.common.DecisionMaker;
 
-import java.util.Optional;
+import java.util.*;
 
 /**
  * Brain that implements Simple player in Legal player's infrastructure
@@ -13,8 +13,21 @@ import java.util.Optional;
 public class SimpleBrains implements DecisionMaker {
     @Override
     public Optional< Move > findBestMove( Position position ) {
-        return position.isTerminal() ?
-                Optional.empty() :
-                Optional.of( position.getMoves().iterator().next() );
+        if ( position.isTerminal() ) {
+            return Optional.empty();
+        }
+
+        List< Move > suggestedMoves = Arrays.asList(
+         new Move( "e2", "e4" ),
+         new Move( "d2", "d4" ),
+         new Move( "e7", "e5" ),
+         new Move( "d7", "d5" ),
+         Move.RESIGN );
+
+        Set< Move > legalMoves = position.getMoves();
+        return Optional.of(
+                suggestedMoves.stream().filter( legalMoves::contains )
+                .findFirst()
+                .orElse( legalMoves.iterator().next() ) );
     }
 }
