c39845e7ad0315b64fde64c8b0812075c403eee0
Leonid Rozenblyum <lrozenblyum@gmail.com>
2017-02-12 21:49:00 +0200

    Multiple moves experiment looks successful. just 2 moves are left to implement.

--- src/main/java/com/leokom/chess/player/simple/SimpleBrains.java
+++ src/main/java/com/leokom/chess/player/simple/SimpleBrains.java
@@ -6,6 +6,9 @@ import com.leokom.chess.player.legal.evaluator.common.DecisionMaker;
 
 import java.util.*;
 
+import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
+
 /**
  * Brain that implements Simple player in Legal player's infrastructure
  * Created by Leonid on 09.02.17.
@@ -17,16 +20,16 @@ public class SimpleBrains implements DecisionMaker {
             return Collections.emptyList();
         }
 
-        List< List< Move > > suggestedMoves = Arrays.asList(
-            Collections.singletonList(new Move("e2", "e4")),
-            Collections.singletonList(new Move("d2", "d4")),
-            Collections.singletonList(new Move("e7", "e5")),
-            Collections.singletonList(new Move("d7", "d5")),
-            Collections.singletonList(Move.RESIGN));
+        List< List< Move > > suggestedMoves = asList(
+            singletonList(new Move("e2", "e4")),
+            asList(new Move("d2", "d4"), Move.OFFER_DRAW),
+            singletonList(new Move("e7", "e5")),
+            asList(new Move("d7", "d5"), Move.OFFER_DRAW),
+            singletonList(Move.RESIGN));
 
         Set< Move > legalMoves = position.getMoves();
         return suggestedMoves.stream().filter(suggestedMove -> legalMoves.contains(suggestedMove.get(0)))
                 .findFirst()
-                .orElse( Collections.singletonList( legalMoves.iterator().next() ) );
+                .orElse( singletonList( legalMoves.iterator().next() ) );
     }
 }
