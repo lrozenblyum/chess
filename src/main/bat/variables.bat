73bc356d5af3b4de4282fcda4a8e990f18ad956b
Leonid Rozenblyum <lrozenblyum@gmail.com>
2019-02-05 23:30:36 +0200

    Let command line suggest depth 2 explicitly by default.

--- src/main/bat/variables.bat
+++ src/main/bat/variables.bat
@@ -7,7 +7,7 @@ SET RUNNABLE_JAR_DIRECTORY=%WINBOARD_INSTALLATION_PATH%\LeokomChess
 SET JAVA_PATH=Q:\Program Files\Java\jdk1.8.0_162\bin\java.exe
 SET RUN_JAR_PATH=%RUNNABLE_JAR_DIRECTORY%\Chess.jar
 rem you may pass -Dblack.engine=Simple to choose a different engine for blacks
-rem for LegalPlayer you may specify -Dblack.depth
-SET RUN_OPTIONS=
+rem for LegalPlayer you may specify -Dblack.depth (1 or 2)
+SET RUN_OPTIONS=-Dblack.depth=2
 
 SET ENGINE=%JAVA_PATH% %RUN_OPTIONS% -jar %RUN_JAR_PATH%
