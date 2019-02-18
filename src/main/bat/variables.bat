92789ce5552fe1b57bf829e86b82f91ed5a68443
Leonid Rozenblyum <lrozenblyum@gmail.com>
2019-02-18 23:02:10 +0200

    Reordered the bat file: configurable parameters: first

--- src/main/bat/variables.bat
+++ src/main/bat/variables.bat
@@ -1,10 +1,12 @@
+rem the variables should be tuned per target environment
 SET WINBOARD_INSTALLATION_PATH=E:\Games\WinBoard-4.8.0
+SET JAVA_PATH=Q:\Program Files\Java\jdk1.8.0_162\bin\java.exe
+
 rem UI that we use to run our Chess with
 SET WINBOARD_PATH=%WINBOARD_INSTALLATION_PATH%\WinBoard\winboard.exe
 rem I use the Winboard installation as a Chess deployment target
 rem it should be equal to 'project.deployDirectory' property in pom.xml
 SET RUNNABLE_JAR_DIRECTORY=%WINBOARD_INSTALLATION_PATH%\LeokomChess
-SET JAVA_PATH=Q:\Program Files\Java\jdk1.8.0_162\bin\java.exe
 SET RUN_JAR_PATH=%RUNNABLE_JAR_DIRECTORY%\Chess.jar
 rem you may pass -Dblack.engine=Simple to choose a different engine for blacks
 rem for LegalPlayer you may specify -Dblack.depth (1 or 2)
