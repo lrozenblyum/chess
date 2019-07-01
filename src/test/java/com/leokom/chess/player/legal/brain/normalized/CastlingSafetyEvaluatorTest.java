3d2b6a8dd58e3844593fca1557256034083657ab
Leonid Rozenblyum <lrozenblyum@gmail.com>
2019-07-01 22:46:29 +0300

    Test for issue: 2'nd ply evaluator doesn't want castling

--- src/test/java/com/leokom/chess/player/legal/brain/normalized/CastlingSafetyEvaluatorTest.java
+++ src/test/java/com/leokom/chess/player/legal/brain/normalized/CastlingSafetyEvaluatorTest.java
@@ -1,7 +1,6 @@
 package com.leokom.chess.player.legal.brain.normalized;
 
-import com.leokom.chess.engine.Move;
-import com.leokom.chess.engine.Position;
+import com.leokom.chess.engine.*;
 import com.leokom.chess.player.legal.brain.common.EvaluatorAsserts;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,10 +8,12 @@ import org.junit.Test;
 //normalized specific test
 public class CastlingSafetyEvaluatorTest {
     private CastlingSafetyEvaluator evaluator;
+    private TwoPliesEvaluator<Position, Move> twoPliesEvaluator;
 
     @Before
     public void prepare() {
         evaluator = new CastlingSafetyEvaluator();
+        twoPliesEvaluator = new TwoPliesEvaluator<>( this.evaluator );
     }
 
     @Test
@@ -40,10 +41,36 @@ public class CastlingSafetyEvaluatorTest {
     public void twoPliesThinkingLogical() {
         Position position = Position.getInitialPosition();
 
-        TwoPliesEvaluator<Position, Move> twoPliesEvaluator = new TwoPliesEvaluator<>( this.evaluator );
-
         new EvaluatorAsserts( twoPliesEvaluator ).assertFirstBetter( position,
                 new Move( "g1", "f3" ), //more room for castling
                 new Move( "a2", "a3" ) ); //no room for castling
+    }
+
+    @Test
+    public void singlePlyWantsCastling() {
+        Position position = new PositionBuilder()
+                .add(Side.WHITE, "a1", PieceType.ROOK)
+                .add(Side.WHITE, "e1", PieceType.KING)
+                .add(Side.WHITE, "h2", PieceType.PAWN)
+                .add(Side.BLACK, "e8", PieceType.KING)
+                .build();
+
+        new EvaluatorAsserts( this.evaluator ).assertFirstBetter( position,
+                new Move( "e1", "c1" ), //castling
+                new Move( "h2", "h3" ) ); //not castling
+    }
+
+    @Test
+    public void twoPliesWantsCastling() {
+        Position position = new PositionBuilder()
+                .add(Side.WHITE, "a1", PieceType.ROOK)
+                .add(Side.WHITE, "e1", PieceType.KING)
+                .add(Side.WHITE, "h2", PieceType.PAWN)
+                .add(Side.BLACK, "e8", PieceType.KING)
+                .build();
+
+        new EvaluatorAsserts( this.twoPliesEvaluator ).assertFirstBetter( position,
+                new Move( "e1", "c1" ), //castling
+                new Move( "h2", "h3" ) ); //not castling
     }
 }
