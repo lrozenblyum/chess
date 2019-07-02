4a74b7b40ef8e254ad55d1b7343b5c6c624df720
Leonid Rozenblyum <lrozenblyum@gmail.com>
2019-07-02 22:34:44 +0300

    Javadoc

--- src/main/java/com/leokom/chess/player/legal/brain/normalized/Range.java
+++ src/main/java/com/leokom/chess/player/legal/brain/normalized/Range.java
@@ -1,6 +1,8 @@
 package com.leokom.chess.player.legal.brain.normalized;
 
-
+/**
+ * Double Interval (inclusive) [ minValue, maxValue ]
+ */
 class Range {
     private final double minValue;
     private final double maxValue;
@@ -14,6 +16,15 @@ class Range {
         this.maxValue = maxValue;
     }
 
+    /**
+     * Convert value from current range to the target one in proportional manner.
+     *
+     * Current: [ 0, 100 ], Target: [ 10, 20 ], value = 30 -> 13
+     *
+     * @param targetRange target range to put the value into
+     * @param value value inside current range
+     * @return value put proportionally in target range
+     */
     double convert( Range targetRange, double value ) {
         if ( value < this.minValue || value > this.maxValue ) {
             throw new IllegalArgumentException( String.format( "The value %s is out of range [ %s, %s ] ", value, minValue, maxValue ) );
