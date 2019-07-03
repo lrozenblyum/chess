9f87e64df9c80bc5ae14853ff6a2b9a6b172d61d
Leonid Rozenblyum <lrozenblyum@gmail.com>
2019-07-03 22:53:18 +0300

    Additional center control test coverage

--- src/test/java/com/leokom/chess/player/legal/brain/normalized/CenterControlEvaluatorTest.java
+++ src/test/java/com/leokom/chess/player/legal/brain/normalized/CenterControlEvaluatorTest.java
@@ -1,8 +1,11 @@
 package com.leokom.chess.player.legal.brain.normalized;
 
+import com.leokom.chess.engine.Move;
 import com.leokom.chess.engine.Position;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 //normalized specific test
 public class CenterControlEvaluatorTest {
@@ -20,5 +23,13 @@ public class CenterControlEvaluatorTest {
         Position position = Position.getInitialPosition().move( "e2", "e4" );
 
         new NormalizedEvaluatorAssert( evaluator ).assertAllMovesEvaluatedInNormalizedRange( position );
+    }
+
+    @Test
+    public void equalCenterControlZeroAdvantage() {
+        Position position = Position.getInitialPosition()
+                .move( "f2", "f3" );
+
+        assertEquals( 0.5, evaluator.evaluateMove( position, new Move( "e7", "e5" ) ), 0.0 );
     }
 }
