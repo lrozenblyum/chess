62ddd1cc4087a49be98c74809fa123f06968ff01
Leonid Rozenblyum <lrozenblyum@gmail.com>
2019-07-17 22:25:30 +0300

    Protection evaluator respects fact of multiple pieces protecting one.

--- src/main/java/com/leokom/chess/player/legal/brain/denormalized/ProtectionIndexCalculator.java
+++ src/main/java/com/leokom/chess/player/legal/brain/denormalized/ProtectionIndexCalculator.java
@@ -3,16 +3,14 @@ package com.leokom.chess.player.legal.brain.denormalized;
 import com.leokom.chess.engine.Position;
 import com.leokom.chess.engine.Side;
 
-import java.util.Set;
-
 
 final class ProtectionIndexCalculator {
 	private ProtectionIndexCalculator() {}
 
-	static float getIndex(Position targetPosition, Side ourSide ) {
-		Set<String> controlledByUs = targetPosition.getSquaresAttackedBy( ourSide );
-
-		//TODO: very primitive: just checks what's protected and what's not
-		return targetPosition.getSquaresOccupiedBySide( ourSide ).stream().mapToInt( square -> controlledByUs.contains( square ) ? 1 : 0 ).sum();
+	static float getIndex( Position targetPosition, Side ourSide ) {
+		//checks level of protection
+		return targetPosition.getSquaresOccupiedBySide(ourSide).stream().mapToLong(
+				square -> targetPosition.getSquaresAttackingSquare( ourSide, square ).count()
+		).sum();
 	}
 }
