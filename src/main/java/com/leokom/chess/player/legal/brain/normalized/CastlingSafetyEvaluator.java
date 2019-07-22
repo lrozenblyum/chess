b08b16e9c2eea6b1eef7506b1815853cfd411a07
Leonid Rozenblyum <lrozenblyum@gmail.com>
2019-07-22 22:37:23 +0300

    Refactoring: reused common facility

--- src/main/java/com/leokom/chess/player/legal/brain/normalized/CastlingSafetyEvaluator.java
+++ src/main/java/com/leokom/chess/player/legal/brain/normalized/CastlingSafetyEvaluator.java
@@ -6,6 +6,8 @@ import com.leokom.chess.engine.Move;
 import com.leokom.chess.engine.Position;
 import com.leokom.chess.engine.Side;
 import com.leokom.chess.player.legal.brain.common.Evaluator;
+import com.leokom.chess.player.legal.brain.common.SideEvaluator;
+import com.leokom.chess.player.legal.brain.internal.common.SymmetricEvaluator;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -27,9 +29,6 @@ public class CastlingSafetyEvaluator implements Evaluator {
 
 	private static final SymmetricalNormalizedRange RANGE = new SymmetricalNormalizedRange( WORST_MOVE, BEST_MOVE );
 
-	private static final Set< String > FILES_IN_BETWEEN_QUEEN_SIDE = ImmutableSet.of( "b", "c", "d" );
-
-	private static final Set< String > FILES_IN_BETWEEN_KING_SIDE = ImmutableSet.of( "f", "g" );
 
 	/*
 	 * If rook is captured - don't think it would be possible
@@ -46,64 +45,71 @@ public class CastlingSafetyEvaluator implements Evaluator {
 	 */
 	@Override
 	public double evaluateMove( Position position, Move move ) {
-		final Side side = position.getSideToMove();
-
 		Position targetPosition = position.move( move );
 
-		return RANGE.normalize( getCastlingSafetyIndex( targetPosition, side ) - getCastlingSafetyIndex( targetPosition, side.opposite() ) );
+		return RANGE.normalize( new SymmetricEvaluator( new CastlingSafetySideEvaluator() ).evaluate( targetPosition ));
 	}
 
-	private double getCastlingSafetyIndex(Position target, Side side) {
-		//strategy : 'castling addicted player'
-		// avoid moving rook and king
-		//if it's not castling (I want to see castling)
-		//after castling we allow such moves
+	private static class CastlingSafetySideEvaluator implements SideEvaluator {
+		private static final Set< String > FILES_IN_BETWEEN_QUEEN_SIDE = ImmutableSet.of( "b", "c", "d" );
+		private static final Set< String > FILES_IN_BETWEEN_KING_SIDE = ImmutableSet.of( "f", "g" );
 
-		//if anywhere in the history the castling was executed, our goal has been reached
-		if ( target.hasCastlingExecuted( side ) ) {
-			return BEST_MOVE;
+
+		@Override
+		public double evaluatePosition(Position target, Side side) {
+			//strategy : 'castling addicted player'
+			// avoid moving rook and king
+			//if it's not castling (I want to see castling)
+			//after castling we allow such moves
+
+			//if anywhere in the history the castling was executed, our goal has been reached
+			if ( target.hasCastlingExecuted( side ) ) {
+				return BEST_MOVE;
+			}
+
+			//if king has moved already - all other moves don't help in castling safety
+			if ( target.hasKingMoved( side ) ) {
+				return WORST_MOVE;
+			}
+
+			//any of rooks is moving: reducing chances to castle
+			if ( target.hasARookMoved( side ) || target.hasHRookMoved( side ) ) {
+				return WORST_MOVE;
+			}
+
+			//the less occupied - the better
+			//normalizing to range [ 0.25, 0.75 ] of 'acceptable' moves
+			return PIECES_BETWEEN_KING_AND_ROOKS.convert( ACCEPTABLE_MOVES_EVALUATION, getFreeInBetween( target, side ) );
 		}
 
-		//if king has moved already - all other moves don't help in castling safety
-		if ( target.hasKingMoved( side ) ) {
-			return WORST_MOVE;
+
+		private int getFreeInBetween( Position position, Side side ) {
+			return MAX_PIECES_BETWEEN_KING_AND_ROOKS - getOccupiedInBetween( position, side );
 		}
 
-		//any of rooks is moving: reducing chances to castle
-		if ( target.hasARookMoved( side ) || target.hasHRookMoved( side ) ) {
-			return WORST_MOVE;
-		}
+		/**
+		 * Collecting pieces between king and not-moved rook
+		 * @param position position
+		 * @param side side (important to inject to check state AFTER move)
+		 * @return amount of occupied squares between king & not-moved rook
+		 */
+		private int getOccupiedInBetween( Position position, Side side ) {
+			//collecting squares between king and not-moved rook
 
-		//the less occupied - the better
-		//normalizing to range [ 0.25, 0.75 ] of 'acceptable' moves
-		return PIECES_BETWEEN_KING_AND_ROOKS.convert( ACCEPTABLE_MOVES_EVALUATION, getFreeInBetween( target, side ) );
+			int rank = side == Side.WHITE ? 1 : 8;
+			Set< String > squaresInBetween = new HashSet<>();
+			if ( !position.hasARookMoved( side ) ) {
+				FILES_IN_BETWEEN_QUEEN_SIDE.forEach( file -> squaresInBetween.add( file + rank ) );
+			}
+
+			if ( !position.hasHRookMoved( side ) ) {
+				FILES_IN_BETWEEN_KING_SIDE.forEach( file -> squaresInBetween.add( file + rank ) );
+			}
+
+			final Set<String> occupiedInBetween = Sets.intersection( position.getSquaresOccupied(), squaresInBetween );
+
+			return occupiedInBetween.size();
+		}
 	}
 
-	private int getFreeInBetween( Position position, Side side ) {
-		return MAX_PIECES_BETWEEN_KING_AND_ROOKS - getOccupiedInBetween( position, side );
-	}
-
-	/**
-	 * Collecting pieces between king and not-moved rook
-	 * @param position position
-	 * @param side side (important to inject to check state AFTER move)
-	 * @return amount of occupied squares between king & not-moved rook
-	 */
-	private int getOccupiedInBetween( Position position, Side side ) {
-		//collecting squares between king and not-moved rook
-
-		int rank = side == Side.WHITE ? 1 : 8;
-		Set< String > squaresInBetween = new HashSet<>();
-		if ( !position.hasARookMoved( side ) ) {
-			FILES_IN_BETWEEN_QUEEN_SIDE.forEach( file -> squaresInBetween.add( file + rank ) );
-		}
-
-		if ( !position.hasHRookMoved( side ) ) {
-			FILES_IN_BETWEEN_KING_SIDE.forEach( file -> squaresInBetween.add( file + rank ) );
-		}
-
-		final Set<String> occupiedInBetween = Sets.intersection( position.getSquaresOccupied(), squaresInBetween );
-
-		return occupiedInBetween.size();
-	}
 }
