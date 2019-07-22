917894ef30d73320e01f838ae4fdc8dca440b690
Leonid Rozenblyum <lrozenblyum@gmail.com>
2019-07-22 22:23:58 +0300

    Refactoring to the new interface

--- src/main/java/com/leokom/chess/player/legal/brain/denormalized/AttackEvaluator.java
+++ src/main/java/com/leokom/chess/player/legal/brain/denormalized/AttackEvaluator.java
@@ -18,37 +18,41 @@ class AttackEvaluator implements Evaluator {
 	public double evaluateMove( Position position, Move move ) {
 		final Side ourSide = position.getSideToMove();
 		Position targetPosition = position.move(move);
-		return getIndex( targetPosition, ourSide )
-				- getIndex( targetPosition, ourSide.opposite() );
+
+		return new AttackSideEvaluator().evaluatePosition( targetPosition, ourSide ) - new AttackSideEvaluator().evaluatePosition( targetPosition, ourSide.opposite() );
 	}
 
-	/*
-	 * Backlog for improvements:
-	 * - king has become a main target for attacks
-	 */
-	private float getIndex( Position targetPosition, Side side ) {
-		final Set< String > squaresAttacked = getPiecesAttackedBy( targetPosition, side );
-
-		// sum of piece values
-		// if a piece is protected - index of piece value is reduced
-		float result = 0;
-		for ( String attackedSquare : squaresAttacked ) {
-			//REFACTOR: probably bad dependency on another brain - extract common utility
-			int pieceValue = MaterialEvaluator.getValue( targetPosition.getPieceType( attackedSquare ) );
-
-			final Stream< String > protectors = targetPosition.getSquaresAttackingSquare( side.opposite(), attackedSquare );
-
-			//+1 to avoid / 0, more protectors is better
-			result += pieceValue / ( protectors.count() + 1.0 );
+	private static class AttackSideEvaluator implements com.leokom.chess.player.legal.brain.common.SideEvaluator {
+		//TODO: technically, in case of a terminal position, it should return empty result
+		//REFACTOR: too generic to encapsulate into Position?
+		private static Set<String> getPiecesAttackedBy( Position position, Side attackerSide ) {
+			Set<String> defenderSquares = position.getSquaresOccupiedBySide( attackerSide.opposite() );
+			return Sets.intersection( position.getSquaresAttackedBy( attackerSide ),
+					defenderSquares );
 		}
-		return result;
+
+		/*
+		 * Backlog for improvements:
+		 * - king has become a main target for attacks
+		 */
+		@Override
+		public double evaluatePosition(Position position, Side side) {
+			final Set< String > squaresAttacked = getPiecesAttackedBy( position, side );
+
+			// sum of piece values
+			// if a piece is protected - index of piece value is reduced
+			float result = 0;
+			for ( String attackedSquare : squaresAttacked ) {
+				//REFACTOR: probably bad dependency on another brain - extract common utility
+				int pieceValue = MaterialEvaluator.getValue( position.getPieceType( attackedSquare ) );
+
+				final Stream< String > protectors = position.getSquaresAttackingSquare( side.opposite(), attackedSquare );
+
+				//+1 to avoid / 0, more protectors is better
+				result += pieceValue / ( protectors.count() + 1.0 );
+			}
+			return result;
+		}
 	}
 
-	//TODO: technically, in case of a terminal position, it should return empty result
-	//REFACTOR: too generic to encapsulate into Position?
-	private static Set<String> getPiecesAttackedBy( Position position, Side attackerSide ) {
-		Set<String> defenderSquares = position.getSquaresOccupiedBySide( attackerSide.opposite() );
-		return Sets.intersection( position.getSquaresAttackedBy( attackerSide ),
-				defenderSquares );
-	}
 }
