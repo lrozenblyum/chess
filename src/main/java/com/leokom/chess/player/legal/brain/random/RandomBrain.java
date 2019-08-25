4177ae403919cb510b69740eb371ddbfca8c8c83
Leonid Rozenblyum <lrozenblyum@gmail.com>
2019-08-25 22:35:09 +0300

    Simple random brain

--- src/main/java/com/leokom/chess/player/legal/brain/random/RandomBrain.java
+++ src/main/java/com/leokom/chess/player/legal/brain/random/RandomBrain.java
@@ -4,12 +4,29 @@ import com.leokom.chess.engine.Move;
 import com.leokom.chess.engine.Position;
 import com.leokom.chess.player.legal.brain.common.Brain;
 
-import java.util.List;
+import java.util.*;
 
 public class RandomBrain implements Brain {
+    private final Random randomGenerator;
+
+    public RandomBrain() {
+        this( new Random() );
+    }
+
+    private RandomBrain( Random randomGenerator ) {
+        this.randomGenerator = randomGenerator;
+    }
+
     @Override
-    public List<Move> findBestMove(Position position) {
-        return null;
+    public List<Move> findBestMove( Position position ) {
+        int moveToPeek = randomGenerator.nextInt(position.getMoves().size());
+        Iterator<Move> moveIterator = position.getMoves().iterator();
+
+        for ( int moveIndex = 0; moveIndex < moveToPeek; moveIndex++ ) {
+            moveIterator.next();
+        }
+
+        return Collections.singletonList(moveIterator.next());
     }
 
     //at the moment move for the opponent is not generated, although it's possible and may bring
--- src/test/java/com/leokom/chess/player/legal/brain/random/RandomBrainTest.java
+++ src/test/java/com/leokom/chess/player/legal/brain/random/RandomBrainTest.java
@@ -1,12 +1,25 @@
 package com.leokom.chess.player.legal.brain.random;
 
+import com.leokom.chess.engine.Move;
+import com.leokom.chess.engine.Position;
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.hasItem;
 import static org.junit.Assert.*;
 
 public class RandomBrainTest {
     @Test
     public void brainExistence() {
         assertEquals( "RandomBrain", new RandomBrain().name() );
+    }
+
+    @Test
+    public void minimalConsistency() {
+        RandomBrain brain = new RandomBrain();
+        List<Move> bestMoves = brain.findBestMove( Position.getInitialPosition() );
+        assertThat( Position.getInitialPosition().getMoves(), hasItem( bestMoves.get( 0 ) ) );
     }
 }
