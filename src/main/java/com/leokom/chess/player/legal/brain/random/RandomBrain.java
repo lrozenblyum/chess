fda76ddafcebf53ea2098bd255e44275d49f6ad0
Leonid Rozenblyum <lrozenblyum@gmail.com>
2019-08-27 22:23:53 +0300

    Move filter integrated to real code

--- src/main/java/com/leokom/chess/player/legal/brain/random/RandomBrain.java
+++ src/main/java/com/leokom/chess/player/legal/brain/random/RandomBrain.java
@@ -5,24 +5,29 @@ import com.leokom.chess.engine.Position;
 import com.leokom.chess.player.legal.brain.common.Brain;
 
 import java.util.*;
+import java.util.function.UnaryOperator;
 
 public class RandomBrain implements Brain {
     private final RandomMove randomMove;
+    private final UnaryOperator<Set<Move>> moveFilter;
 
     public RandomBrain() {
-        this( new RandomMove( new Random()::nextInt ) );
+        this( new RandomMove( new Random()::nextInt ), new MoveFilter() );
     }
 
-    private RandomBrain( RandomMove randomMove )  {
+    private RandomBrain(RandomMove randomMove, UnaryOperator<Set< Move >> moveFilter )  {
         this.randomMove = randomMove;
+        this.moveFilter = moveFilter;
     }
 
     @Override
     public List<Move> findBestMove( Position position ) {
         return
-            randomMove.select( position.getMoves() )
-                .map( Collections::singletonList )
-                .orElse( Collections.emptyList() );
+            randomMove.select(
+                moveFilter.apply( position.getMoves() )
+            )
+            .map( Collections::singletonList )
+            .orElse( Collections.emptyList() );
     }
 
     //at the moment move for the opponent is not generated, although it's possible and may bring
