d90d2352e702f5da6f3d3cbbffbb0c0e93971c54
Leonid Rozenblyum <lrozenblyum@gmail.com>
2019-08-28 21:44:22 +0300

    Javadoc

--- src/main/java/com/leokom/chess/player/legal/brain/random/RandomBrain.java
+++ src/main/java/com/leokom/chess/player/legal/brain/random/RandomBrain.java
@@ -7,6 +7,17 @@ import com.leokom.chess.player.legal.brain.common.Brain;
 import java.util.*;
 import java.util.function.UnaryOperator;
 
+/**
+ * The brain supports selection of a random move from the set of legal moves in a position.
+ * Following moves are excluded:
+ * <ul>
+ *     <li>RESIGN - because we don't want to give up fast</li>
+ *     <li>OFFER_DRAW - because https://github.com/lrozenblyum/chess/issues/161 is not solved yet.
+ *     Simulation has shown that our LegalPlayer simply exits in such situation</li>
+ * </ul>
+ *
+ * CONFIRM_DRAW is not excluded because it seems reasonable to keep it as a possibility.
+ */
 public class RandomBrain implements Brain {
     private final RandomMove randomMove;
     private final UnaryOperator<Set<Move>> moveFilter;
