f7ac3dfe86c6b1af65cb696da35f4194bad321ee
Leonid Rozenblyum <lrozenblyum@gmail.com>
2019-08-28 21:39:24 +0300

    Refactoring: generalized set of unwanted moves.

--- src/main/java/com/leokom/chess/player/legal/brain/random/MoveFilter.java
+++ src/main/java/com/leokom/chess/player/legal/brain/random/MoveFilter.java
@@ -1,5 +1,6 @@
 package com.leokom.chess.player.legal.brain.random;
 
+import com.google.common.collect.ImmutableSet;
 import com.leokom.chess.engine.Move;
 
 import java.util.Set;
@@ -7,12 +8,12 @@ import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
 
 class MoveFilter implements UnaryOperator< Set<Move> >  {
+    private static final Set<Move> UNWANTED_MOVES = ImmutableSet.of( Move.OFFER_DRAW, Move.RESIGN );
 
     @Override
     public Set<Move> apply(Set<Move> moves) {
         return moves.stream().
-                filter( move -> !move.equals( Move.RESIGN ) ).
-                filter( move -> !move.equals( Move.OFFER_DRAW ) ).
-                collect(Collectors.toSet() );
+                filter( move -> ! UNWANTED_MOVES.contains( move ) ).
+                collect( Collectors.toSet() );
     }
 }
