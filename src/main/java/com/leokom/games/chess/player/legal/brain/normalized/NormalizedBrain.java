cbf94892dd0ce9c46f198e7518831ee76b1f227d
Leonid Rozenblyum <lrozenblyum@gmail.com>
2019-09-24 13:16:54 +0300

    Javadoc + constant

--- src/main/java/com/leokom/games/chess/player/legal/brain/normalized/NormalizedBrain.java
+++ src/main/java/com/leokom/games/chess/player/legal/brain/normalized/NormalizedBrain.java
@@ -8,11 +8,17 @@ import com.leokom.games.commons.brain.normalized.GenericNormalizedBrain;
 
 import java.util.List;
 
+/**
+ * Normalized chess brain.
+ * It depends on the fact all the evaluators return their values in normalized range.
+ * Draw offer is not supported by this brain (it does best efforts to avoid generating it as the suggested move).
+ */
 public class NormalizedBrain implements GenericBrain<Position, Move> {
+    private static final int DEFAULT_THINKING_DEPTH_PLIES = 1;
     private final GenericNormalizedBrain<Position, Move> engine;
 
     public NormalizedBrain(Evaluator evaluator) {
-        this( evaluator, 1 );
+        this( evaluator, DEFAULT_THINKING_DEPTH_PLIES);
     }
 
     public NormalizedBrain(Evaluator evaluator, int depth) {
