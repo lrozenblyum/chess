ce428d55c0868550bc0b6dc5a64b9a5064fed983
Leonid Rozenblyum <lrozenblyum@gmail.com>
2020-03-31 23:12:02 +0300

    Consistent naming

--- src/test/java/com/leokom/games/chess/PGNGameTest.java
+++ src/test/java/com/leokom/games/chess/PGNGameTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
-public class PGNTest {
+public class PGNGameTest {
     @Test
     public void eventTag() {
         String pgn = new PGNGame(new Event(null)).run();
