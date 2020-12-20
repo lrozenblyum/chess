092318c69f9a5313fbd772c207e21a9f2bf74aa2
Leonid Rozenblyum <lrozenblyum@gmail.com>
2020-12-20 17:00:25 +0200

    Result tag: black player win dilemma.
    Should Game provide better representation of the game result?

--- src/test/java/com/leokom/games/chess/pgn/PGNGameTest.java
+++ src/test/java/com/leokom/games/chess/pgn/PGNGameTest.java
@@ -104,5 +104,15 @@ public class PGNGameTest {
         assertEquals( "[Result \"1-0\"]", pgn.split( "\n" )[ 6 ] );
     }
 
+    @Test
+    public void blackWins() {
+        Game spyGame = Mockito.spy(new Game(whitePlayer, blackPlayer));
+        Mockito.doReturn( blackPlayer ).when( spyGame ).run();
+
+        String pgn = new PGNGame(new Event(null, null, null), spyGame ).run();
+
+        assertEquals( "[Result \"0-1\"]", pgn.split( "\n" )[ 6 ] );
+    }
+
 
 }
